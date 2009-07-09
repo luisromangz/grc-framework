92822b51cb2cedeba4dacaf1b199581724472a91
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2009-07-09 18:42:06 +0000

    · ConfigurablePageController handleRequestInternal method has been made final. Further customization of the request handling must be done by overriding the customHandleRequest method. · LoginController has been imported (from SensisWeb) and made to extend ConfigurablePageController. · InstallController has been adapted to its parent's handleRequestInternal  method now being final.

---
+++ GreenRiverCommonsSpring/src/com/greenriver/commons/mvc/controllers/LoginController.java
@@ -0,0 +1,35 @@
+package com.greenriver.commons.mvc.controllers;
+
+import com.greenriver.commons.mvc.helpers.header.HeaderConfigurer;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author luis
+ */
+public class LoginController extends ConfigurablePageController {
+
+    public LoginController() {
+    }
+
+    @Override
+    protected void customHandleRequest(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            ModelAndView modelAndView) throws Exception {
+        
+
+        boolean authError =  String.valueOf(
+                request.getParameter("login_error")).equals("1");
+        boolean accessDenied =  String.valueOf(
+                request.getParameter("access_denied")).equals("1");
+        boolean sessionExpired =  String.valueOf(
+                request.getParameter("session_expired")).equals("1");
+
+        modelAndView.addObject("error",  authError);
+        modelAndView.addObject("accessDenied",  accessDenied);
+        modelAndView.addObject("sessionExpired", sessionExpired);
+    }
+}
