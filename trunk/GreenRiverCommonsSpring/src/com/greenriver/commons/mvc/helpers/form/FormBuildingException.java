aaa24a6792ab2b0f6cfc36ce97154c6c453f6524
mangelp <mangelp@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2009-07-16 06:55:21 +0000

    - Added exception to notify errors found while building a form. - Added some checks to the Form builder to ensure that no unnecesary options are specified and that when the type is a selection there is defined a source for the list of elements to show. - Other minor changes.

---
+++ trunk/GreenRiverCommonsSpring/src/com/greenriver/commons/mvc/helpers/form/FormBuildingException.java
@@ -0,0 +1,31 @@
+/*#################################################################
+(c)2008/2009 Greenriver Consulting SL. All rights reserved.
+
+This project and all subsequent files are licensed under the terms
+and conditions of the GreenRiverLicense.
+
+If you haven't received a copy of the license you can obtain it at
+http://www.greenriverconsulting.es/licensing/greenriverlicense
+
+Author: mangelp
+###################################################################*/
+
+package com.greenriver.commons.mvc.helpers.form;
+
+/**
+ * Exception caused by a problem found while building a form.
+ */
+public class FormBuildingException extends RuntimeException {
+
+    public FormBuildingException() {
+	super();
+    }
+
+    public FormBuildingException(String msg) {
+	super(msg);
+    }
+
+    public FormBuildingException(String msg, Throwable t) {
+	super(msg, t);
+    }
+}
