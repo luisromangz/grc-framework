fb4a8ffb8bb1c6e9ea615cc04d6c22db180a0954
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2009-07-16 17:06:01 +0000

    · Form configuration is no longer done just with a list of the entity classes forms are to be generated for, but a map, in which the key is the id of said forms. Created forms' ids are of the form 'id' when used with ConfigurablePageController, and 'taskName_id'  when used with PageTasksController and created through tasks. Ids are no longer converted to lowercase.

--- trunk/GreenRiverCommonsSpring/src/com/greenriver/commons/mvc/controllers/PageConfiguration.java
+++ trunk/GreenRiverCommonsSpring/src/com/greenriver/commons/mvc/controllers/PageConfiguration.java
@@ -3,7 +3,9 @@ package com.greenriver.commons.mvc.controllers;
 
 import com.greenriver.commons.mvc.helpers.header.PageHeaderConfiguration;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
 
 /**
  * This class extends <c>PageHeaderConfiguration</c> in order to be able
@@ -13,12 +15,12 @@ import java.util.List;
 public class PageConfiguration extends PageHeaderConfiguration
         implements FormsConfiguration {
 
-     private List<String> formEntities;
+     private Map<String,String> formEntities;
 
      public PageConfiguration () {
          super();
 
-         formEntities = new ArrayList<String>();
+         formEntities = new Hashtable<String, String>();
      }
 
     /**
@@ -26,7 +28,7 @@ public class PageConfiguration extends PageHeaderConfiguration
      * of their instances.
      * @return the formEntities
      */
-    public List<String> getFormEntities() {
+    public Map<String,String> getFormEntities() {
         return formEntities;
     }
 
@@ -34,16 +36,17 @@ public class PageConfiguration extends PageHeaderConfiguration
      * Sets the names of the entities a form will be created for.
      * @param formEntities the formEntities to set
      */
-    public void setFormEntities(List<String> formEntities) {
-        this.formEntities = formEntities;
+    public void setFormEntities(Map<String,String> formEntities) {
+        this.formEntities = new Hashtable<String, String>(formEntities);
     }
 
     /**
      * Adds an entity name to the list of entity names for which forms have
      * to be created.
+     * @param id The form's id.
      * @param entityName The name of the entity a form will be created for.
      */
-    public void addFormEntity(String entityName) {
-        this.formEntities.add(entityName);
+    public void addFormEntity(String id, String entityName) {
+        this.formEntities.put(id, entityName);
     }
 }
