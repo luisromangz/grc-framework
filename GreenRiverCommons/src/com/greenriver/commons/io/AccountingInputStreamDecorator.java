63275907ff36e51863052708e494457fb4c55ab6
mangelp <mangelp@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2009-07-18 14:00:59 +0000

    - Added more methods to Dates utility. - Improved decorators for byte accounting. Added overloads to reset the count and added synchronize keywork to decorators to ensure that operations over the byte count are atomic. - Fixed issues with how the read bytes where added and with some methods not counting the bytes read/write.

--- GreenRiverCommons/src/com/greenriver/commons/io/AccountingInputStreamDecorator.java
+++ GreenRiverCommons/src/com/greenriver/commons/io/AccountingInputStreamDecorator.java
@@ -9,7 +9,6 @@ http://www.greenriverconsulting.es/licensing/greenriverlicense
 
 Author: mangelp
 ###################################################################*/
-
 package com.greenriver.commons.io;
 
 import java.io.IOException;
@@ -19,94 +18,114 @@ import java.io.InputStream;
  * Decorator for an input stream that accounts the number of bytes read.
  */
 public class AccountingInputStreamDecorator extends InputStream
-        implements AccountedStream {
+	implements AccountedStream {
 
     private InputStream decorated;
     private long readCount;
 
     public InputStream getDecorated() {
-        return decorated;
+	return decorated;
     }
 
-    public long getByteCount() {
-        return readCount;
+    public synchronized long getByteCount() {
+	return readCount;
+    }
+
+    public synchronized long getByteCount(boolean reset) {
+	long result = readCount;
+
+	if (reset) {
+	    readCount = 0L;
+	}
+
+	return result;
     }
 
     public AccountingInputStreamDecorator(InputStream decorated) {
-        this.decorated = decorated;
+	this.decorated = decorated;
     }
 
     @Override
-    public int read() throws IOException {
-        int result = decorated.read();
-        readCount++;
-        return result;
+    public synchronized int read() throws IOException {
+	int result = decorated.read();
+	if (result > 0) {
+	    readCount++;
+	}
+	return result;
     }
 
     @Override
     public int available() throws IOException {
-        try {
-            return decorated.available();
-        } catch (IOException ex) {
-            throw ex;
-        }
+	try {
+	    return decorated.available();
+	} catch (IOException ex) {
+	    throw ex;
+	}
     }
 
     @Override
     public void close() throws IOException {
-        decorated.close();
+	decorated.close();
     }
 
     @Override
     public boolean equals(Object obj) {
-        return decorated.equals(obj);
+	return decorated.equals(obj);
     }
 
     @Override
     public int hashCode() {
-        return decorated.hashCode();
+	return decorated.hashCode();
     }
 
     @Override
     public synchronized void mark(int readlimit) {
-        decorated.mark(readlimit);
+	decorated.mark(readlimit);
     }
 
     @Override
     public boolean markSupported() {
-        return decorated.markSupported();
+	return decorated.markSupported();
     }
 
     @Override
-    public int read(byte[] b) throws IOException {
-        return read(b, 0, b.length);
+    public synchronized int read(byte[] b) throws IOException {
+	int result = decorated.read(b, 0, b.length);
+	if (result > 0) {
+	    readCount += result;
+	}
+	return result;
     }
 
     @Override
-    public int read(byte[] b, int off, int len) throws IOException {
-        return decorated.read(b, off, len);
+    public synchronized int read(byte[] b, int off, int len) throws IOException {
+	int result = decorated.read(b, off, len);
+	if (result > 0) {
+	    readCount += result;
+	}
+	return result;
     }
 
     @Override
-    public synchronized void reset() throws IOException {
-        decorated.reset();
+    public void reset() throws IOException {
+	decorated.reset();
     }
 
+    /**
+     * Sets the stream that is decorated
+     * @param decorated
+     */
     public void setDecorated(InputStream decorated) {
-        this.decorated = decorated;
-    }
-
-    public void setReadCount(long readCount) {
-        this.readCount = readCount;
+	this.decorated = decorated;
     }
 
     @Override
     public long skip(long n) throws IOException {
-        return decorated.skip(n);
+	return decorated.skip(n);
     }
 
     @Override
     public String toString() {
-        return decorated.toString();
+	return decorated.toString();
     }
 }
