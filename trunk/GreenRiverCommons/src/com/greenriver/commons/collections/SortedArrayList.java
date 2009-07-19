ff5e5ce3ee38aa9988d66a0105b76c12931ad9cf
mangelp <mangelp@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2009-07-19 12:30:07 +0000

    - Added a test for SortedArrayList and done some changes to this class. - Moved Lists utility to collections namespace. Client code would need to change namespace.

--- trunk/GreenRiverCommons/src/com/greenriver/commons/collections/SortedArrayList.java
+++ trunk/GreenRiverCommons/src/com/greenriver/commons/collections/SortedArrayList.java
@@ -9,7 +9,6 @@ http://www.greenriverconsulting.es/licensing/greenriverlicense
 
 Author: mangelp
 ###################################################################*/
-
 package com.greenriver.commons.collections;
 
 import java.util.ArrayList;
@@ -23,75 +22,106 @@ import java.util.Comparator;
  * are not guaranteed to keep the relative order (sorting is not stable).
  *
  * This collection is not thread-safe.
- * @param <T> Type of the list to sort
+ *
+ * When no comparator is specified the sorting is done with the comparable
+ * implementation by the elements of the array. If they don't implement this
+ * an exception is thrown.
+ * @param <T> Type of the elements of the list
  */
 public class SortedArrayList<T> extends ArrayList<T> {
+
     private static final long serialVersionUID = 6731719380450650517L;
 
-    private class DefaultComparator implements Comparator<T> {
+    /**
+     * Comparator that checks if the items implements comparable and then it
+     * uses that for comparison. This comparator is used to call the binary
+     * search method of Collections without having for force the type T to
+     * implement Comparator. This way this list can be used with any kind of
+     * element if you supply the appropiate comparator. If not you will get
+     * an exception if the elements doesn't implement comparable.
+     * @param <T>
+     */
+    private class ComparableComparator<T> implements Comparator<T> {
 
-        public int compare(T o1, T o2) {
-            if (o1 instanceof Comparable) {
-                return ((Comparable)o1).compareTo(o2);
-            } else {
-                throw new IllegalStateException("The type is doesn't " +
-                        "implements Comparable and " +
-                        "you haven't provided a Comparator implementation");
-            }
-        }
+	public int compare(T o1, T o2) {
+	    if (o1 instanceof Comparable) {
+		return ((Comparable)o1).compareTo(o2);
+	    } else {
+		throw new IllegalArgumentException("Not comparable elements");
+	    }
+	}
     }
 
     private Comparator<T> comparator;
+    private ComparableComparator<T> fallbackComparator;
+
+    /**
+     * Gets the comparator in use
+     * @return
+     */
+    public Comparator<T> getComparator() {
+	return comparator;
+    }
 
     public SortedArrayList() {
-        comparator = new DefaultComparator();
+	super();
     }
 
     public SortedArrayList(int initialCapacity) {
-        super(initialCapacity);
-        comparator = new DefaultComparator();
+	super(initialCapacity);
     }
 
     public SortedArrayList(Comparator<T> comparator) {
-        this.comparator = comparator;
+	super();
+	this.comparator = comparator;
     }
 
     public SortedArrayList(int initialCapacity, Comparator<T> comparator) {
-        super(initialCapacity);
-        this.comparator = comparator;
+	super(initialCapacity);
+	this.comparator = comparator;
     }
 
     @Override
     public boolean add(T e) {
-        int pos = 0;
-        if (e instanceof Comparable) {
-            pos = Collections.binarySearch(this, e, comparator);
-        }
+	int pos = 0;
 
-        if (pos < 0) {
-            pos = - (pos + 1);
-        }
+	if (comparator == null && !(e instanceof Comparable)) {
+	    throw new IllegalStateException("The type doesn't " +
+		    "implements Comparable and " +
+		    "you haven't provided a Comparator implementation");
+	}
 
-        if (pos == size()) {
-            return super.add(e);
-        } else {
-            super.add(pos, e);
-            return true;
-        }
+	if (comparator != null) {
+	    pos = Collections.binarySearch(this, e, comparator);
+	} else {
+	    //We need to use the fallback comparator here as we can't call this
+	    //method without forcing in the generic type definition of T that
+	    //it must implement Comparable
+	    pos = Collections.binarySearch(this, e, fallbackComparator);
+	}
+
+	if (pos < 0) {
+	    pos = -(pos + 1);
+	}
+
+	if (pos == size()) {
+	    return super.add(e);
+	} else {
+	    super.add(pos, e);
+	    return true;
+	}
     }
 
     @Override
     public boolean addAll(Collection<? extends T> collection) {
 
-       boolean result = false;
-       for(T element : collection) {
-            if(this.add(element)) {
-                result = true;
-            }            
-        }
+	boolean result = false;
+	for (T element : collection) {
+	    if (this.add(element)) {
+		result = true;
+	    }
+	}
 
-        return result;
+	return result;
     }
-
-    
 }
