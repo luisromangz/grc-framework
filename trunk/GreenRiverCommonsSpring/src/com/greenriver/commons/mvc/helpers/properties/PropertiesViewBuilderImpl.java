967a0f2b335203041f843d2afa5137a8994cb33f
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2009-07-23 14:02:04 +0000

    · Added margin to the unit-holding propertyView span.

--- trunk/GreenRiverCommonsSpring/src/com/greenriver/commons/mvc/helpers/properties/PropertiesViewBuilderImpl.java
+++ trunk/GreenRiverCommonsSpring/src/com/greenriver/commons/mvc/helpers/properties/PropertiesViewBuilderImpl.java
@@ -2,7 +2,6 @@ package com.greenriver.commons.mvc.helpers.properties;
 
 import com.greenriver.commons.Strings;
 import com.greenriver.commons.data.fieldProperties.FieldProperties;
-import com.greenriver.commons.mvc.helpers.HtmlElementInfo;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -21,156 +20,158 @@ public class PropertiesViewBuilderImpl implements PropertiesViewBuilder {
     private PropertiesView currentPropertiesView;
 
     public PropertiesViewBuilderImpl() {
-	propertiesViews = new ArrayList<PropertiesView>();
+        propertiesViews = new ArrayList<PropertiesView>();
     }
 
     private void assertCurrent() {
-	if (currentPropertiesView == null) {
-	    throw new IllegalStateException("No properties view have been added to this builder");
-	}
+        if (currentPropertiesView == null) {
+            throw new IllegalStateException(
+                    "No properties view have been added to this builder");
+        }
     }
 
     public PropertiesView addPropertiesView(String id, ModelAndView mav) {
-	if (mav.getModel().containsKey(id)) {
-	    throw new IllegalArgumentException("There is already a properties view with the id " + id);
-	}
+        if (mav.getModel().containsKey(id)) {
+            throw new IllegalArgumentException(
+                    "There is already a properties view with the id " + id);
+        }
 
-	currentPropertiesView = new PropertiesView(id);
-	mav.addObject(id, currentPropertiesView);
+        currentPropertiesView = new PropertiesView(id);
+        mav.addObject(id, currentPropertiesView);
 
-	return currentPropertiesView;
+        return currentPropertiesView;
     }
 
     public List<PropertiesView> getPropertiesViews() {
-	return Collections.unmodifiableList(propertiesViews);
+        return Collections.unmodifiableList(propertiesViews);
     }
 
     public PropertiesView getCurrentPropertiesView() {
-	return currentPropertiesView;
+        return currentPropertiesView;
     }
 
     public void makeCurrent(PropertiesView propertiesView) {
-	if (propertiesView == null) {
-	    throw new IllegalArgumentException(
-		    "The properties view can't be null");
-	}
+        if (propertiesView == null) {
+            throw new IllegalArgumentException(
+                    "The properties view can't be null");
+        }
 
-	if (!propertiesViews.contains(propertiesView)) {
-	    throw new IllegalArgumentException(
-		    "The properties view is not defined in the builder");
-	}
+        if (!propertiesViews.contains(propertiesView)) {
+            throw new IllegalArgumentException(
+                    "The properties view is not defined in the builder");
+        }
 
-	this.currentPropertiesView = propertiesView;
+        this.currentPropertiesView = propertiesView;
     }
 
     public SinglePropertyView addPropertyView(String id,
-	    FieldProperties properties,
-	    Class modelClass) {
+            FieldProperties properties,
+            Class modelClass) {
 
-	if (properties == null) {
-	    throw new IllegalArgumentException("Properties can't be null");
-	}
+        if (properties == null) {
+            throw new IllegalArgumentException("Properties can't be null");
+        }
 
-	if (Strings.isNullOrEmpty(id)) {
-	    throw new IllegalArgumentException("Id can't be null nor empty.");
-	}
+        if (Strings.isNullOrEmpty(id)) {
+            throw new IllegalArgumentException("Id can't be null nor empty.");
+        }
 
-	assertCurrent();
+        assertCurrent();
 
-	if (!properties.visible()) {
-	    return null;
-	}
+        if (!properties.visible()) {
+            return null;
+        }
 
-	SinglePropertyView propView = new SinglePropertyView(
-		this.currentPropertiesView.getId() + "_" + id);
-	propView.setLabel(properties.label());
+        SinglePropertyView propView = new SinglePropertyView(
+                this.currentPropertiesView.getId() + "_" + id);
+        propView.setLabel(properties.label());
 
-	if (setupPropertyView(propView, properties, modelClass)) {
-	    this.currentPropertiesView.addPropertyView(propView);
-	}
+        if (setupPropertyView(propView, properties, modelClass)) {
+            this.currentPropertiesView.addPropertyView(propView);
+        }
 
-	return propView;
+        return propView;
     }
 
     public void addPropertyViewsFromModel(String entityFullName) {
-	addPropertyViewsFromModel(entityFullName, null);
+        addPropertyViewsFromModel(entityFullName, null);
     }
 
     public void addPropertyViewsFromModel(String entityFullName, List<String> propertiesToShow) {
-	Class modelClass = null;
+        Class modelClass = null;
 
-	try {
-	    modelClass = Class.forName(entityFullName);
-	} catch (ClassNotFoundException ex) {
-	    throw new IllegalArgumentException(
-		    "Entity class '" + entityFullName + "' can't be loaded.",
-		    ex);
-	}
+        try {
+            modelClass = Class.forName(entityFullName);
+        } catch (ClassNotFoundException ex) {
+            throw new IllegalArgumentException(
+                    "Entity class '" + entityFullName + "' can't be loaded.",
+                    ex);
+        }
 
-	addPropertyViewsFromModel(modelClass, propertiesToShow);
+        addPropertyViewsFromModel(modelClass, propertiesToShow);
     }
 
     public void addPropertyViewsFromModel(Class modelClass) {
-	addPropertyViewsFromModel(modelClass, null);
+        addPropertyViewsFromModel(modelClass, null);
     }
 
     public void addPropertyViewsFromModel(Class modelClass, List<String> propertiesToShow) {
-	assertCurrent();
+        assertCurrent();
 
-	Field[] classFields = modelClass.getDeclaredFields();
+        Field[] classFields = modelClass.getDeclaredFields();
 
-	for (Field field : classFields) {
+        for (Field field : classFields) {
 
-	    if (propertiesToShow != null &&
-		    !propertiesToShow.contains(field.getName())) {
-		continue;
-	    }
+            if (propertiesToShow != null &&
+                    !propertiesToShow.contains(field.getName())) {
+                continue;
+            }
 
-	    FieldProperties props = field.getAnnotation(FieldProperties.class);
-	    if (props != null && props.visible()) {
-		this.addPropertyView(field.getName(), props, field.getType());
-	    }
-	}
+            FieldProperties props = field.getAnnotation(FieldProperties.class);
+            if (props != null && props.visible()) {
+                this.addPropertyView(field.getName(), props, field.getType());
+            }
+        }
     }
 
     public void addPropertyViewFromConfiguration(Map<String, Object> config) {
-	if (!config.containsKey(key_model)) {
-	    throw new IllegalArgumentException(
-		    "The configuration must include '" + key_model +
-		    "' key with the entity full name as value");
-	}
+        if (!config.containsKey(key_model)) {
+            throw new IllegalArgumentException(
+                    "The configuration must include '" + key_model +
+                    "' key with the entity full name as value");
+        }
 
-	Object obj = config.get(key_model);
-	String entityName = null;
-	List<String> properties = null;
+        Object obj = config.get(key_model);
+        String entityName = null;
+        List<String> properties = null;
 
-	if (obj == null || !(obj instanceof String)) {
-	    throw new IllegalArgumentException(
-		    "The configuration must include '" + key_model +
-		    "' key with the entity full name as value");
-	}
+        if (obj == null || !(obj instanceof String)) {
+            throw new IllegalArgumentException(
+                    "The configuration must include '" + key_model +
+                    "' key with the entity full name as value");
+        }
 
-	entityName = (String) obj;
-	obj = config.get(key_properties);
+        entityName = (String) obj;
+        obj = config.get(key_properties);
 
-	if (obj != null && !(obj instanceof List)) {
-	    throw new IllegalArgumentException(
-		    "The configuration includes key '" + key_properties +
-		    "' but the value is not a list.");
-	} else if (obj != null) {
-	    properties = (List<String>) obj;
-	    addPropertyViewsFromModel(entityName, properties);
-	} else {
-	    addPropertyViewsFromModel(entityName);
-	}
+        if (obj != null && !(obj instanceof List)) {
+            throw new IllegalArgumentException(
+                    "The configuration includes key '" + key_properties +
+                    "' but the value is not a list.");
+        } else if (obj != null) {
+            properties = (List<String>) obj;
+            addPropertyViewsFromModel(entityName, properties);
+        } else {
+            addPropertyViewsFromModel(entityName);
+        }
     }
 
     public void removePropertyView(String id) {
-	assertCurrent();
+        assertCurrent();
 
-	SinglePropertyView propView = new SinglePropertyView(
-		currentPropertiesView.getId() + "_" + id);
-	currentPropertiesView.removePropertyView(propView);
+        SinglePropertyView propView = new SinglePropertyView(
+                currentPropertiesView.getId() + "_" + id);
+        currentPropertiesView.removePropertyView(propView);
     }
 
     /**
@@ -185,13 +186,13 @@ public class PropertiesViewBuilderImpl implements PropertiesViewBuilder {
      * discarded.
      */
     private boolean setupPropertyView(SinglePropertyView propView,
-	    FieldProperties properties, Class modelClass) {
+            FieldProperties properties, Class modelClass) {
 
-	switch (properties.type()) {
-	    //TODO: Concrete initiallization for anybody?
-	    default:
-		return setupFieldGenericView(propView, properties, modelClass);
-	}
+        switch (properties.type()) {
+            //TODO: Concrete initiallization for anybody?
+            default:
+                return setupFieldGenericView(propView, properties, modelClass);
+        }
     }
 
     /**
@@ -202,24 +203,25 @@ public class PropertiesViewBuilderImpl implements PropertiesViewBuilder {
      * @return
      */
     private boolean setupFieldGenericView(SinglePropertyView propView,
-	    FieldProperties properties,
-	    Class modelClass) {
-	//all the view are the same, an span for the value and another one
-	//for the unit
-	String format =
-		"<span id=\"%1$s\"></span><span id=\"%1$s_unit\">%2$s</span>";
-	String unit = properties.unit();
-	//If no unit put an empty string
-	if (unit == null) {
-	    unit = "";
-	}
+            FieldProperties properties,
+            Class modelClass) {
+        //all the view are the same, an span for the value and another one
+        //for the unit
+        String format = "<span id=\"%1$s\"></span>";
+        format +=
+                "<span style=\"margin-left:5px\" id=\"%1$s_unit\">%2$s</span>";
 
-	propView.setValueElement(
-		String.format(format, propView.getId(), unit));
+        String unit = properties.unit();
+        //If no unit put an empty string
+        if (unit == null) {
+            unit = "";
+        }
 
-	return true;
+        propView.setValueElement(
+                String.format(format, propView.getId(), unit));
+
+        return true;
     }
-
     //TODO: To handle more or less complex setups for other properties
     //add new methods here with the same parameters as the setupPropertiesView
     //method.
