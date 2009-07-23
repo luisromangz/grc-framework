8b99473ca206a4d019bc86a9cbce53aee01e51ac
mangelp <mangelp@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2009-07-23 09:46:58 +0000

    - Custom implementation of dwr's access control that extends the default one to add a workaround about a bug in dwr: http://bugs.directwebremoting.org/bugs/browse/DWR-307 - Added reference to dwr library.

---
+++ trunk/GreenRiverCommonsSpring/src/com/greenriver/commons/dwr/MultiRoleDwrAccessControl.java
@@ -0,0 +1,35 @@
+
+package com.greenriver.commons.dwr;
+
+import com.greenriver.commons.Strings;
+import java.util.Set;
+import org.directwebremoting.extend.AccessControl;
+import org.directwebremoting.impl.DefaultAccessControl;
+
+
+/**
+ * Extends default access control to add support to specify more than one role
+ * in the same dwr:auth tag tp overcome a bug in dwr 2-3.RC1.
+ * The multiple roles are specified separating them by commas.
+ * @author Miguel Angel
+ */
+public class MultiRoleDwrAccessControl extends DefaultAccessControl implements AccessControl {
+
+    @Override
+    public void addRoleRestriction(String scriptName, String methodName, String role) {
+	String[] roles = role.split(",");
+
+	for (String allowedRole : roles) {
+	    allowedRole = allowedRole.trim();
+	    if (!Strings.isNullOrEmpty(allowedRole)) {
+		super.addRoleRestriction(scriptName, methodName, allowedRole);
+	    }
+	}
+    }
+
+    @Override
+    protected Set<String> getRoleRestrictions(String scriptName, String methodName) {
+	Set<String> result = super.getRoleRestrictions(scriptName, methodName);
+	return result;
+    }
+}
