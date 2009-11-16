c75e30b03601648f4e9e1c5b5f2499b27147012b
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2009-11-16 13:00:19 +0000

    · Added annotation FieldDeactivationCondition for declariing when a field should be activated based on other entity's fields values. · Added parameter to FieldProperties annotation so FieldDeactivationCondition behaves as a nested annotation.

---
+++ trunk/GreenRiverCommons/src/com/greenriver/commons/data/fieldProperties/FieldDeactivationCondition.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.greenriver.commons.data.fieldProperties;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @author luis
+ */
+@Documented
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FieldDeactivationCondition {
+    public String widgetId();
+    public String equals() default "";
+    public String notEquals() default "";
+}
