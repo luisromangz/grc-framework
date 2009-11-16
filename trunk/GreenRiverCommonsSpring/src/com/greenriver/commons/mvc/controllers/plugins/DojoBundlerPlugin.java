5af9d5b18ef82a470b549d897897960380982d3c
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2009-11-16 17:44:09 +0000

    · Refactored the old DojoBundlerPlugin into BaseBundlerPlugin, which gets extended by the new DojoBundlerPlugin and JavascriptBundlerPlugin.

--- trunk/GreenRiverCommonsSpring/src/com/greenriver/commons/mvc/controllers/plugins/DojoBundlerPlugin.java
+++ trunk/GreenRiverCommonsSpring/src/com/greenriver/commons/mvc/controllers/plugins/DojoBundlerPlugin.java
@@ -1,105 +1,50 @@
+
 package com.greenriver.commons.mvc.controllers.plugins;
 
-// <editor-fold defaultstate="collapsed" desc="Imports">
-import com.greenriver.commons.collections.Lists;
 import com.greenriver.commons.mvc.configuration.PageConfiguration;
-import com.yahoo.platform.yui.compressor.JavaScriptCompressor;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.regex.MatchResult;
-import javax.servlet.http.HttpServletRequest;
-import org.mozilla.javascript.EvaluatorException;
-import org.mozilla.javascript.tools.ToolErrorReporter;
-import org.springframework.security.providers.encoding.PasswordEncoder;
-// </editor-fold>
 
 /**
  *
  * @author luis
  */
-public class DojoBundlerPlugin implements ControllerPlugin {
+public class DojoBundlerPlugin extends BaseBundlerPlugin {
 
-    // <editor-fold defaultstate="collapsed" desc="Fields">
-    private String javascriptBasePath;
-    private boolean alwaysCreate;
-    private PasswordEncoder passwordEncoder;
     private List<String> excludedNamespaces;
-    private boolean applyCompression;
-    // </editor-fold>
 
-    public DojoBundlerPlugin() {
-        this.excludedNamespaces = new ArrayList<String>();
+    public DojoBundlerPlugin () {
+         this.excludedNamespaces = new ArrayList<String>();
+         this.setBundlePrefix("dojo");
     }
 
-    public void doWork(HttpServletRequest request, PageConfiguration configuration) {
-        if (javascriptBasePath == null) {
-            javascriptBasePath = String.format(
-                    "%s/js/",
-                    request.getSession().getServletContext().getRealPath(""));
-        }
+    @Override
+    protected List<String> getFileNames(PageConfiguration configuration) {
+        return configuration.getDojoModules();
+    }
 
-        List<String> dojoModules = configuration.getDojoModules();
+   
 
-        String bundleName = Lists.join(dojoModules, ",");
-
-        // We encode the bundle's name, so if modules change, the name
-        // of the bundled file will change too, and thus will force a new
-        // bundle generation.
-        bundleName = passwordEncoder.encodePassword(bundleName, null);
-        bundleName = "dojo-" + bundleName;
-
-        String bundlePath = String.format(
-                "%sdojo/%s.js",
-                javascriptBasePath,
-                bundleName);
-
-        File bundleFile = new File(bundlePath);
-        if (bundleFile.exists() && !alwaysCreate) {
-            configuration.getDojoBundles().clear();
-            //configuration.addDojoBundle(bundleName);
-            configuration.addDojoBundle(
-                    bundleName + (applyCompression ? ".js.compressed" : ""));
-            return;
-        }
-
-        addDojoModules(dojoModules, bundleFile);
-
-
+    @Override
+    protected void addBundle(String bundleName,PageConfiguration configuration) {
         // When using the auto bundler, we discard other bundles.
         configuration.getDojoBundles().clear();
-
-        if (applyCompression) {
-            FileReader reader;
-            try {
-                reader = new FileReader(bundlePath);
-                JavaScriptCompressor compressor = new JavaScriptCompressor(
-                        reader,
-                        new ToolErrorReporter(false));
-                FileWriter compressedWriter =
-                        new FileWriter(bundlePath + ".compressed.js");
-                compressor.compress(compressedWriter, 1000, false, false, false,
-                        false);
-
-                compressedWriter.close();
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            } catch (EvaluatorException ex) {
-                throw new RuntimeException(ex);
-            }
-        }
-
-        configuration.addDojoBundle(
-                bundleName + (applyCompression ? ".js.compressed" : ""));
+        configuration.addDojoBundle(bundleName);
     }
 
-    public void addDojoModules(List<String> newModules, File bundleFile) {
+    @Override
+    protected void bundleFiles(PageConfiguration configuration, File bundleFile) {
+        addDojoModules(configuration.getDojoModules(), bundleFile);
+    }
+
+     public void addDojoModules(List<String> newModules, File bundleFile) {
         List<String> loadedModules = new ArrayList<String>();
 
         try {
@@ -139,7 +84,7 @@ public class DojoBundlerPlugin implements ControllerPlugin {
     }
 
     public String getPathFromModuleName(String moduleName) {
-        return this.javascriptBasePath + moduleName.replace('.', '/') + ".js";
+        return this.getJavascriptBasePath()+"/" + moduleName.replace('.', '/') + ".js";
     }
 
     public void processModuleFile(
@@ -155,7 +100,7 @@ public class DojoBundlerPlugin implements ControllerPlugin {
 
 
         File moduleFile = new File(getPathFromModuleName(moduleName));
-        Scanner scanner;
+        Scanner scanner = null;
         try {
             scanner = new Scanner(moduleFile);
         } catch (FileNotFoundException ex) {
@@ -164,7 +109,7 @@ public class DojoBundlerPlugin implements ControllerPlugin {
         List<String> lines = new ArrayList<String>();
         // We start reading the module file
         while (scanner.hasNextLine()) {
-            
+
             String nextLine = scanner.nextLine();
 
             // We search for a declare statement in the line.
@@ -207,35 +152,7 @@ public class DojoBundlerPlugin implements ControllerPlugin {
         scanner.close();
     }
 
-    // <editor-fold defaultstate="collapsed" desc="Getters and setters">
-    /**
-     * @return the alwaysCreate
-     */
-    public boolean isAlwaysCreate() {
-        return alwaysCreate;
-    }
-
-    /**
-     * @param alwaysCreate the alwaysCreate to set
-     */
-    public void setAlwaysCreate(boolean alwaysCreate) {
-        this.alwaysCreate = alwaysCreate;
-    }
-
-    /**
-     * @return the passwordEncoder
-     */
-    public PasswordEncoder getPasswordEncoder() {
-        return passwordEncoder;
-    }
-
-    /**
-     * @param passwordEncoder the passwordEncoder to set
-     */
-    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
-    }
-
+    // <editor-fold defaultstate="collapsed" desc="Getters & setters">
     /**
      * @return the excludedNamespaces
      */
@@ -250,18 +167,6 @@ public class DojoBundlerPlugin implements ControllerPlugin {
         this.excludedNamespaces = excludedNamespaces;
     }
 
-    /**
-     * @return the getApplyCompression
-     */
-    public boolean getApplyCompression() {
-        return applyCompression;
-    }
-
-    /**
-     * @param getApplyCompression the getApplyCompression to set
-     */
-    public void setApplyCompression(boolean applyCompression) {
-        this.applyCompression = applyCompression;
-    }
+    
     // </editor-fold>
 }
---
+++ trunk/GreenRiverCommonsSpring/src/com/greenriver/commons/mvc/controllers/plugins/JavascriptBundlerPlugin.java
@@ -0,0 +1,74 @@
+package com.greenriver.commons.mvc.controllers.plugins;
+
+import com.greenriver.commons.mvc.configuration.PageConfiguration;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ *
+ * @author luis
+ */
+public class JavascriptBundlerPlugin extends BaseBundlerPlugin {
+
+    public JavascriptBundlerPlugin () {
+        this.setBundlePrefix("js");
+    }
+
+    @Override
+    protected List<String> getFileNames(PageConfiguration configuration) {
+        return configuration.getJavaScriptFiles();
+    }
+
+    @Override
+    protected void addBundle(String bundleName, PageConfiguration configuration) {
+        configuration.getJavaScriptFiles().clear();
+        configuration.addJavaScriptFile(bundleName);
+    }
+
+    @Override
+    protected void bundleFiles(PageConfiguration configuration, File bundleFile) {
+        BufferedWriter writer = null;
+        try {
+            writer = new BufferedWriter(new FileWriter(bundleFile));
+            for (String fileName : configuration.getJavaScriptFiles()) {
+                addFileToBundle(fileName, writer);
+            }
+            writer.close();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        } finally {
+            try {
+                writer.close();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+    }
+
+    private void addFileToBundle(String fileName, BufferedWriter writer)
+            throws IOException {
+        BufferedReader reader =
+                new BufferedReader(
+                new FileReader(
+                String.format("%s/%s.js", this.getJavascriptBasePath(), fileName)));
+
+        writer.newLine();
+        writer.write("// File: "+fileName);
+        writer.newLine();
+
+        while (true) {
+            String line = reader.readLine();
+            if (line == null) {
+                break;
+            }
+
+            writer.write(line);
+            writer.newLine();
+        }
+    }
+}
