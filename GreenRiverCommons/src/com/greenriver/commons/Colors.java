fcfc2427cd7d58de9f50795733788b0155a877c3
mangelp <mangelp@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2009-12-09 20:04:17 +0000

    - Added a Base64 impl that i found and which is not licensed (public domain).

--- GreenRiverCommons/src/com/greenriver/commons/Colors.java
+++ GreenRiverCommons/src/com/greenriver/commons/Colors.java
@@ -1,14 +1,3 @@
-/*#################################################################
-(c)2008/2009 Greenriver Consulting SL. All rights reserved.
-
-This project and all subsequent files are licensed under the terms
-and conditions of the GreenRiverLicense.
-
-If you haven't received a copy of the license you can obtain it at
-http://www.greenriverconsulting.es/licensing/greenriverlicense
-
-Author: mangelp
-###################################################################*/
 
 package com.greenriver.commons;
 
@@ -18,6 +7,7 @@ import java.util.List;
 
 /**
  * Color utilities
+ * @author Miguel Angel
  */
 public class Colors {
 
