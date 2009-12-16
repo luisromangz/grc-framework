d1d5cdb42e94aaade3703a42d7fac58c454b6c2e
mangelp <mangelp@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2009-12-16 17:17:29 +0000

    - Made a constants public and changed the max and min values to better fit common sizes. - Changed exception thrown in not implemented CIFValidator.

--- trunk/GreenRiverCommons/src/com/greenriver/commons/security/CIFValidator.java
+++ trunk/GreenRiverCommons/src/com/greenriver/commons/security/CIFValidator.java
@@ -1,13 +1,11 @@
 
 package com.greenriver.commons.security;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 /**
  * @author Miguel Angel
  */
 public class CIFValidator {
     public boolean validate(String cif) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException();
     }
 }
