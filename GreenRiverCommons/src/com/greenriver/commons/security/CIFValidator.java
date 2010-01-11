0efa327aa5f03a3c5601eee1b4411281362c308b
mangelp <mangelp@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2010-01-11 12:46:16 +0000

    - Added lazy validation for cif and nif fields at once. For now only the format of the field is validated and not the error checking code. - Some changes to the meal schedule editor trying to fix a bug that causes the stay period and selected biologist to be cleared when we go back to edit the stay period when creating a new meal schedule. The stay period now is in place, but the biologist is cleared somewhere else :S

--- GreenRiverCommons/src/com/greenriver/commons/security/CIFValidator.java
+++ GreenRiverCommons/src/com/greenriver/commons/security/CIFValidator.java
@@ -5,6 +5,9 @@ package com.greenriver.commons.security;
  * @author Miguel Angel
  */
 public class CIFValidator {
+
+    public static String CIF_LETTERS = "ABCDEFGHJKLMNPQRSUVW";
+    
     public boolean validate(String cif) {
         throw new UnsupportedOperationException();
     }
