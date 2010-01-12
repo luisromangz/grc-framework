940ae210dccb1d52ba1985410db107d71c0187de
mangelp <mangelp@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2010-01-12 13:59:15 +0000

    - The total price is now shown in the last step of the meal schedule editor but there are some issues left that makes the cost appear instead of the price in the movements and in other places. - The private rounding method that was in MenuCostsCalculationCriteria is now in MealScheduleDtoPriceCalculator. - Created MealScheduleDtoToDtoConverter to trim all the not selected stuff from the dto received from the client. - The view of the meals/dishes selection now shows the use of the menu for each meal plus the hint icon. - Client-side of cif or nif fields now validates properly. Server-side is pending.

--- trunk/GreenRiverCommons/src/com/greenriver/commons/security/CIFValidator.java
+++ trunk/GreenRiverCommons/src/com/greenriver/commons/security/CIFValidator.java
@@ -7,6 +7,8 @@ package com.greenriver.commons.security;
 public class CIFValidator {
 
     public static String CIF_LETTERS = "ABCDEFGHJKLMNPQRSUVW";
+    public static String VALIDATION_LETTERS = "OABCDEFGHIJ";
+    public static String NUMERIC_CHECK_LETTERS = "ABDEFGHJUV";
     
     public boolean validate(String cif) {
         throw new UnsupportedOperationException();
