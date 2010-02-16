eef7afa4b120456cc756d039ce5495442d2f2b56
mangelp <mangelp@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2010-02-16 13:36:07 +0000

    - HibernateMealScheduleDaoImpl uses the support for generating criterias from annotated query arguments. Also removed MealScheduleHqlQueryBuilder as it is unused. - Refactored the code for generating criterias out of HibernateBaseDao to CriteriaFactoryImpl class and also created an interface so this can be conveniently wired up with spring (in a future). - Refactored existing pagination code (PagedDataQuery and MySqlPagedDataQueryImpl) into an interface with the basic operations and two implementations. One that uses criterias to do all the work (is the default one as the scroll thing its a bit hacky) and another that uses a forward only scroll to get the total number of results (it's said to be more efficient in mysql). Also integrated with CriteriaFactory impl as it needs already built criterias. - Added support for unit-testing daos loading a customized hibernate configuration in the setup of the testing class and also added some basic tests for HibernateMealScheduleDaoImpl that were helpful to detect some errors. The tests are run against an empty in-memory database using hsqldb but the configuration to run them against a mysql db is also provided (commented out).

---
+++ trunk/GreenRiverCommonsHibernate/src/com/greenriver/commons/data/dao/hibernate/CriteriaFactoryImpl.java
@@ -0,0 +1,218 @@
+
+package com.greenriver.commons.data.dao.hibernate;
+
+import com.greenriver.commons.DateRange;
+import com.greenriver.commons.Strings;
+import com.greenriver.commons.data.dao.queryArguments.EntityQueryArguments;
+import com.greenriver.commons.data.dao.queryArguments.QueryArgumentsFieldProperties;
+import com.greenriver.commons.data.dao.queryArguments.QueryArgumentsProperties;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Date;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Disjunction;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+
+/**
+ * Criteria factory impl that references the dao that is using it.
+ * @author Miguel Angel
+ */
+public class CriteriaFactoryImpl implements CriteriaFactory {
+
+    private Session session;
+
+    @Override
+    public Session getSession() {
+        return session;
+    }
+
+    public void setSession(Session session) {
+        this.session = session;
+    }
+
+    public CriteriaFactoryImpl() {
+    }
+
+    CriteriaFactoryImpl(Session session) {
+        this.session = session;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="QueryArguments related methods">
+    @SuppressWarnings("unchecked")
+    @Override
+    public Criteria createCriteriaFromQueryArguments(
+            EntityQueryArguments queryArguments) {
+
+        Class argumentClass = queryArguments.getClass();
+        QueryArgumentsProperties queryProperties =
+                (QueryArgumentsProperties) argumentClass.getAnnotation(
+                QueryArgumentsProperties.class);
+
+        if (queryProperties == null) {
+            throw new RuntimeException(
+                    "The passed query argument is not annotated with @QueryArgumentsProperties");
+        }
+
+        // The criteria for the target class is created
+        Criteria crit = getSession().createCriteria(
+                queryProperties.targetClass());
+
+        setBasicCriteriaParameters(crit, queryArguments, queryProperties);
+
+        // For the fields of the query argument annotated with @QueryArgumentsFieldProperties
+        for (Field queryArgumentField : argumentClass.getDeclaredFields()) {
+            addFieldRestriction(crit, queryArgumentField, queryArguments);
+        }
+
+        return crit;
+    }
+
+    @Override
+    public Criteria createPaginatedCriteriaFromQueryArguments(
+            int page,
+            int pageSize,
+            EntityQueryArguments queryArguments) {
+        Criteria crit = createCriteriaFromQueryArguments(queryArguments);
+        // We set the pagination values
+        crit.setMaxResults(pageSize);
+        crit.setFirstResult(page * pageSize);
+
+        return crit;
+    }
+
+    private Object getValueForField(
+            Field queryArgumentField,
+            EntityQueryArguments queryArguments) {
+        String methodName = "get" + Strings.toUpperCase(
+                queryArgumentField.getName(), 0, 1);
+        Method accesorMethod = null;
+        Object value = null;
+        try {
+            accesorMethod = queryArguments.getClass().getMethod(methodName);
+            value = accesorMethod.invoke(queryArguments);
+        } catch (IllegalAccessException ex) {
+            throw new RuntimeException(ex);
+        } catch (IllegalArgumentException ex) {
+            throw new RuntimeException(ex);
+        } catch (InvocationTargetException ex) {
+            throw new RuntimeException(ex);
+        } catch (NoSuchMethodException ex) {
+            throw new RuntimeException(ex);
+        } catch (SecurityException ex) {
+            throw new RuntimeException(ex);
+        }
+        return value;
+    }
+
+    private void setBasicCriteriaParameters(
+            Criteria crit,
+            EntityQueryArguments queryArguments,
+            QueryArgumentsProperties queryProperties) {
+
+
+        // We manage the sorting field.
+        if (!Strings.isNullOrEmpty(queryArguments.getSortFieldName())) {
+            if (queryArguments.isSortAscending()) {
+                crit.addOrder(Order.asc(queryArguments.getSortFieldName()));
+            } else {
+                crit.addOrder(Order.desc(queryArguments.getSortFieldName()));
+            }
+        }
+
+        // We add a disjuntion of likes for the fields affected by the text filter.
+        if (!Strings.isNullOrEmpty(queryArguments.getTextFilter())) {
+            Disjunction disjunction = Restrictions.disjunction();
+
+            for (String fieldName : queryProperties.textFilterFields()) {
+                disjunction.add(Restrictions.ilike(
+                        fieldName,
+                        queryArguments.getTextFilter(), MatchMode.ANYWHERE));
+            }
+
+            crit.add(disjunction);
+        }
+    }
+
+    private void addFieldRestriction(
+            Criteria crit,
+            Field queryArgumentField,
+            EntityQueryArguments queryArguments) {
+        QueryArgumentsFieldProperties queryFieldProperties =
+                queryArgumentField.getAnnotation(
+                QueryArgumentsFieldProperties.class);
+        if (queryFieldProperties == null) {
+            // We skip non annotated fields
+            return;
+        }
+
+        Object value = getValueForField(queryArgumentField, queryArguments);
+        if (value == null) {
+            // We dont apply restrictions for null values.
+            return;
+        }
+
+        String fieldName = queryFieldProperties.fieldName();
+        // We add a condition based on the field specified comparison type.
+        switch (queryFieldProperties.type()) {
+            case EQUALS:
+                crit.add(Restrictions.eq(fieldName, value));
+                break;
+            case GREATER_EQUALS:
+                crit.add(Restrictions.ge(fieldName, value));
+                break;
+            case GREATER_THAN:
+                crit.add(Restrictions.gt(fieldName, value));
+                break;
+            case LOWER_EQUALS:
+                crit.add(Restrictions.le(fieldName, value));
+                break;
+            case LOWER_THAN:
+                crit.add(Restrictions.lt(fieldName, value));
+                break;
+            case LIKE:
+                crit.add(Restrictions.ilike(fieldName, value.toString(),
+                        MatchMode.ANYWHERE));
+                break;
+            case IN_RANGE:
+                this.addInRangeFieldRestriction(fieldName, value, crit);
+                break;
+            default:
+                throw new IllegalStateException("Type " +
+                        queryFieldProperties.type() + " not handled properly.");
+        }
+    }
+
+    private void addInRangeFieldRestriction(String fieldName, Object value, Criteria crit) {
+        DateRange range = null;
+
+        if (value instanceof DateRange) {
+            range = (DateRange) value;
+        } else if (value instanceof Date) {
+            range = new DateRange((Date) value);
+        } else {
+            throw new IllegalStateException(
+                    "In range comparison type specified for a value that"
+                    + " is not a date nor a date range.");
+        }
+
+        if (range.getMax() != null) {
+            crit.add(Restrictions.le(fieldName, range.getMax()));
+        } else if (range.getMin() != null) {
+            crit.add(Restrictions.ge(fieldName, range.getMin()));
+        }
+    }
+
+    @Override
+    public Criteria createCountingCriteriaFromQueryArguments(EntityQueryArguments entityQueryArguments) {
+        Criteria crit = createCriteriaFromQueryArguments(entityQueryArguments);
+        crit.setProjection(Projections.rowCount());
+
+        return crit;
+    }
+    // </editor-fold>
+}
---
+++ trunk/GreenRiverCommonsHibernate/src/com/greenriver/commons/data/dao/hibernate/pagination/AbstractCriteriaPagingHelper.java
@@ -0,0 +1,110 @@
+package com.greenriver.commons.data.dao.hibernate.pagination;
+
+import com.greenriver.commons.data.PagedResult;
+import com.greenriver.commons.data.dao.hibernate.CriteriaFactory;
+import com.greenriver.commons.data.dao.queryArguments.EntityQueryArguments;
+import java.util.ArrayList;
+import org.hibernate.Criteria;
+
+/**
+ * @author Miguel Angel
+ */
+public abstract class AbstractCriteriaPagingHelper implements CriteriaPagingHelper {
+
+    private CriteriaFactory criteriaFactory;
+    private EntityQueryArguments entityQueryArguments;
+
+    @Override
+    public CriteriaFactory getCriteriaFactory() {
+        return criteriaFactory;
+    }
+
+    @Override
+    public void setCriteriaFactory(CriteriaFactory criteriaFactory) {
+        this.criteriaFactory = criteriaFactory;
+    }
+
+    @Override
+    public EntityQueryArguments getEntityQueryArguments() {
+        return this.entityQueryArguments;
+    }
+
+    @Override
+    public void setEntityQueryArguments(EntityQueryArguments entityQueryArguments) {
+        this.entityQueryArguments = entityQueryArguments;
+    }
+
+    /**
+     * 
+     * @param critFactory
+     * @param args
+     */
+    public AbstractCriteriaPagingHelper(CriteriaFactory critFactory, EntityQueryArguments args) {
+        this.criteriaFactory = critFactory;
+        this.entityQueryArguments = args;
+    }
+
+    /**
+     * Sets the results, the total page count and also sets the page number
+     * in the result to the last available page if the requested page didn't
+     * existed.
+     * @param pagedResult
+     * @param crit 
+     * @param pageCount
+     * @param pageNumber
+     * @param pageSize
+     */
+    protected void setResults(PagedResult pagedResult, Criteria crit, int pageCount, int pageNumber, int pageSize) {
+        pagedResult.setTotalPages(pageCount);
+        // If there are results to return go ahead, if not return the last
+        // available page of data or nothing when there are no results available
+        if (pageCount > 0 && pageCount > pageNumber * pageSize) {
+            crit.setMaxResults(pageSize);
+            crit.setFirstResult(pageNumber * pageSize);
+            pagedResult.setResult(crit.list());
+            pagedResult.setPageNumber(pageNumber);
+        } else if (pageCount > 0) {
+            pagedResult.setPageNumber(pagedResult.getTotalPages() - 1);
+            crit.setMaxResults(pageSize);
+            crit.setFirstResult(pagedResult.getPageNumber() * pageSize);
+            pagedResult.setResult(crit.list());
+        } else {
+            pagedResult.setResult(new ArrayList(0));
+        }
+    }
+
+    /**
+     * Gets the count of results
+     * @param pageSize 
+     * @return
+     */
+    protected abstract int getPageCount(int pageSize);
+
+    /**
+     * Gets a criteria ready to return results
+     * @param pageNumber
+     * @param pageSize
+     * @return
+     */
+     protected Criteria getListCriteria(int pageNumber, int pageSize) {
+         CriteriaFactory critFactory = this.getCriteriaFactory();
+         EntityQueryArguments entQueryArgs = this.getEntityQueryArguments();
+         return critFactory.createPaginatedCriteriaFromQueryArguments(
+                 pageNumber,
+                 pageSize,
+                 entQueryArgs);
+    }
+
+    @Override
+    public PagedResult list(int pageNumber, int pageSize) {
+        PagedResult pagedResult = new PagedResult();
+        int pageCount = getPageCount(pageSize);
+
+        Criteria crit = getListCriteria(pageNumber, pageSize);
+        
+        setResults(pagedResult, crit, pageCount, pageNumber, pageSize);
+
+        return pagedResult;
+    }
+    
+}
