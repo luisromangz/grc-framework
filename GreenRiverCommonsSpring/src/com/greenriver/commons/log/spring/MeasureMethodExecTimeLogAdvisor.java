54c1fbd5a97ab6f207b6bb6d6441ce7478d412b8
mangelp <mangelp@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2010-02-17 13:57:52 +0000

    - Minor improvement to print method arguments types

--- GreenRiverCommonsSpring/src/com/greenriver/commons/log/spring/MeasureMethodExecTimeLogAdvisor.java
+++ GreenRiverCommonsSpring/src/com/greenriver/commons/log/spring/MeasureMethodExecTimeLogAdvisor.java
@@ -1,7 +1,10 @@
 
 package com.greenriver.commons.log.spring;
 
+import com.greenriver.commons.Strings;
 import com.greenriver.commons.log.Log4jLogger;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 
@@ -23,13 +26,27 @@ public class MeasureMethodExecTimeLogAdvisor
             execTime = System.currentTimeMillis() - execTime;
             Logger logger = this.getLogger();
             logger.debug(String.format(
-                    "Profiled method %1$s::%2$s, execution time: %3$s ms.",
+                    "Profiled %1$s::%2$s(%3$s), execution: %4$s ms.",
                     pjp.getSignature().getDeclaringTypeName(),
                     pjp.getSignature().toShortString(),
+                    getArgsAsString(pjp.getArgs()),
                     execTime));
+
         }
 
         return result;
+    }
+
+    private String getArgsAsString(Object [] args) {
+        List<String> result = new ArrayList<String>();
+        for (Object arg : args) {
+            if (arg == null) {
+                result.add("NULL");
+            } else {
+                result.add(arg.getClass().getSimpleName());
+            }
+        }
+        return Strings.join(result, ", ");
     }
 
     @Override
