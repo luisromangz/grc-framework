7f8a61edc1d8d0ee65843036e40ec7ca2cea2742
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2010-05-06 15:17:52 +0000

    · Added base class for dao class that work with unique entities.

---
+++ GreenRiverCommonsHibernate/src/com/greenriver/commons/data/dao/hibernate/HibernateUniqueResultDao.java
@@ -0,0 +1,35 @@
+package com.greenriver.commons.data.dao.hibernate;
+
+import java.lang.reflect.ParameterizedType;
+import org.hibernate.Criteria;
+
+/**
+ *
+ * @author luis
+ */
+public class HibernateUniqueResultDao<T>
+        extends HibernateDaoBase {
+
+    public T get() {
+
+        // We need the type used as template.
+        ParameterizedType parameterizedType =
+                (ParameterizedType) getClass().getGenericSuperclass();
+
+
+        Criteria crit = getCurrentSession().createCriteria(
+                (Class)parameterizedType.getActualTypeArguments()[0]);
+
+        return (T) crit.uniqueResult();
+    }
+
+    public void save(T entity) {
+        T existingEntity = get();
+        if(existingEntity!=null) {
+            // There can only be one!
+            getCurrentSession().delete(existingEntity);
+        }
+        getCurrentSession().save(entity);
+
+    }
+}
