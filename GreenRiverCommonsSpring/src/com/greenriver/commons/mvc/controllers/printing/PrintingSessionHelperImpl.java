cb768a31118b19c3ab4c71e0d151c10bde0430c2
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2010-06-28 19:10:03 +0000

    · Added support for printing several printableDocuments to a PDF file.

--- GreenRiverCommonsSpring/src/com/greenriver/commons/mvc/controllers/printing/PrintingSessionHelperImpl.java
+++ GreenRiverCommonsSpring/src/com/greenriver/commons/mvc/controllers/printing/PrintingSessionHelperImpl.java
@@ -1,34 +1,46 @@
 package com.greenriver.commons.mvc.controllers.printing;
 
 import com.greenriver.commons.templating.PrintableDocument;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author luisro
  */
 public class PrintingSessionHelperImpl implements PrintingSessionHelper {
-    private PrintableDocument document;
+
+    private List<PrintableDocument> documents;
 
     /**
      * @return the document
      */
     @Override
-    public PrintableDocument getDocument() {
-        return document;
+    public List<PrintableDocument> getDocuments() {
+        return documents;
     }
 
     /**
      * @param document the document to set
      */
     @Override
-    public void setDocument(PrintableDocument document) {
-        this.document = document;
+    public void setDocuments(List<PrintableDocument> documents) {
+        this.documents = documents;
     }
 
     @Override
-    public void clearDocument(){
-        document = null;
+    public void clearDocuments() {
+        documents = new ArrayList<PrintableDocument>();
     }
 
-    
+    @Override
+    public void addDocument(PrintableDocument document) {
+        documents.add(document);
+    }
+
+    @Override
+    public void setDocument(PrintableDocument document) {
+        this.clearDocuments();
+        this.addDocument(document);
+    }
 }
