f5ec7e53825fb6f873859e074474849b8d54f003
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2010-07-05 19:03:49 +0000

    · Simplfied non-subtemplated replacements.

--- trunk/GreenRiverCommons/src/com/greenriver/commons/templating/TemplatingUtils.java
+++ trunk/GreenRiverCommons/src/com/greenriver/commons/templating/TemplatingUtils.java
@@ -12,28 +12,31 @@ public class TemplatingUtils {
         return (replacement instanceof SubtemplatedReplacement);
     }
 
-
     public static String formatTemplateReplacement(TemplateReplacement replacement, String value) {
-        if(value==null) {
+        if (value == null) {
             return "No definido";
         }
 
-        if(Strings.isNullOrEmpty(value)) {
+        value = value.trim();
+
+        if (Strings.isNullOrEmpty(value)) {
             // We dont' return the empty string so, e.g. an empty cell doesn't
             // desappear.
             return "<!--Not empty-->";
         }
 
-        String element="span";
-        if(isSubtemplatedReplacement(replacement)
-                && ((SubtemplatedReplacement)replacement).getSubtemplateField()!=null){
-            element="div";
+        if (isSubtemplatedReplacement(replacement)
+                && ((SubtemplatedReplacement) replacement).getSubtemplateField() != null) {
+            String element = "div";
+
+            return String.format("<%s class=\"%s\">%s</%s>",
+                    "div",
+                    replacement.getPlaceholder(),
+                    value, element);
+        } else {
+            return value;
         }
 
-        return String.format("<%s class=\"%s\">%s</%s>",
-                element,
-                replacement.getPlaceholder(),
-                value,element);
-    }
 
+    }
 }
