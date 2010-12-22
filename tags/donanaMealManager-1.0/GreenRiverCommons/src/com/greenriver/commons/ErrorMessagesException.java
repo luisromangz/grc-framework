4086d8c81854cdfbfdeca0dcbfda6bf38f0d6024
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2010-12-22 12:46:34 +0000

    * Improvements.

--- tags/donanaMealManager-1.0/GreenRiverCommons/src/com/greenriver/commons/ErrorMessagesException.java
+++ tags/donanaMealManager-1.0/GreenRiverCommons/src/com/greenriver/commons/ErrorMessagesException.java
@@ -1,10 +1,11 @@
 package com.greenriver.commons;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 /**
- *
+ * This exception is used to carry multiple error messages instead of just one.
  * @author luis
  */
 public class ErrorMessagesException extends Exception {
@@ -18,10 +19,30 @@ public class ErrorMessagesException extends Exception {
         this.messages = messages;
     }
 
+    public ErrorMessagesException() {
+    }
+
     /**
      * @return the messages
      */
     public List<String> getMessages() {
         return messages;
+    }
+
+    public void addMessage(String message) {
+        if(!messages.contains(message)) {
+            messages.add(message);
+        }
+        
+    }
+
+    public void addMessages(Collection<String> messages) {
+        for(String message : messages) {
+            addMessage(message);
+        }
+    }
+
+    public boolean hasErrors() {
+        return !messages.isEmpty();
     }
 }
