4e5857851aac7f04ecb5773f9ef4e54bde335695
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2011-07-21 09:15:38 +0000

    * Added UserFormDto. * Improvements to the user managementService.

--- GreenRiverCommonsSpring/src/com/greenriver/commons/web/services/userManagement/UserManagementServiceImpl.java
+++ GreenRiverCommonsSpring/src/com/greenriver/commons/web/services/userManagement/UserManagementServiceImpl.java
@@ -1,6 +1,5 @@
 package com.greenriver.commons.web.services.userManagement;
 
-import com.greenriver.commons.Strings;
 import com.greenriver.commons.collections.Applicable;
 import com.greenriver.commons.collections.Lists;
 import com.greenriver.commons.data.dao.UserDao;
@@ -27,8 +26,8 @@ public class UserManagementServiceImpl
     private PasswordEncoder passwordEncoder;
     private FieldsValidator fieldsValidator;
     private RoleManager roleManager;
-    private UserDtoFactory formDtoFactory;
-    private UserDtoFactory dtoFactory;
+    private UserDtoFactory<UserFormDto> formDtoFactory;
+    private UserDtoFactory<UserDto> dtoFactory;
     // </editor-fold>
 
     // <editor-fold defaultstate="collapsed" desc="Getters & setters">
@@ -51,8 +50,36 @@ public class UserManagementServiceImpl
     public void setRoleManager(RoleManager roleManager) {
         this.roleManager = roleManager;
     }
-    // </editor-fold>
 
+    /**
+     * @return the formDtoFactory
+     */
+    public UserDtoFactory<UserFormDto> getFormDtoFactory() {
+        return formDtoFactory;
+    }
+
+    /**
+     * @param formDtoFactory the formDtoFactory to set
+     */
+    public void setFormDtoFactory(UserDtoFactory<UserFormDto> formDtoFactory) {
+        this.formDtoFactory = formDtoFactory;
+    }
+
+    /**
+     * @return the dtoFactory
+     */
+    public UserDtoFactory<UserDto> getDtoFactory() {
+        return dtoFactory;
+    }
+
+    /**
+     * @param dtoFactory the dtoFactory to set
+     */
+    public void setDtoFactory(UserDtoFactory<UserDto> dtoFactory) {
+        this.dtoFactory = dtoFactory;
+    }
+
+    // </editor-fold>
     // <editor-fold defaultstate="collapsed" desc="Service methods">
     @Override
     public Result getNewUser() {
@@ -62,68 +89,33 @@ public class UserManagementServiceImpl
         newUser.setUsername("");
         newUser.setPassword("");
         newUser.setRoles(new String[]{"ROLE_USER"});
-        r.setResult(formDtoFactory.create(newUser));
+        r.setResult(getFormDtoFactory().create(newUser));
         return r;
     }
 
     @Override
-    public Result<UserDto> getForForm(Long userId) {
-        Result<UserDto> r = new Result<UserDto>();
+    public Result<UserFormDto> getForForm(Long userId) {
+        Result<UserFormDto> r = new Result<UserFormDto>();
 
-        try {
-            r.setResult(formDtoFactory.create(userDao.getById(userId)));
-        } catch (RuntimeException e) {
-            r.addErrorMessage("Ocurrió un error en la base de datos.");
+        User user = getUserById(userId, r);
+        if (!r.isSuccess()) {
+            return r;
         }
 
+        r.setResult(formDtoFactory.create(user));
         return r;
     }
 
     @Override
     public Result<UserDto> get(Long userId) {
-
         Result<UserDto> r = new Result<UserDto>();
-
-        try {
-            r.setResult(dtoFactory.create(userDao.getById(userId)));
-        } catch (RuntimeException e) {
-            r.addErrorMessage("Ocurrió un error en la base de datos.");
+        User user = getUserById(userId, r);
+        if (!r.isSuccess()) {
+            return r;
         }
 
+        r.setResult(dtoFactory.create(user));
         return r;
-    }
-
-    private boolean validateUserSaving(UserDto userDto, Result result) {
-        FieldsValidationResult validationResult = fieldsValidator.validate(
-                userDto);
-
-        if (!validationResult.isValid()) {
-            result.setSuccess(false);
-            result.addErrorMessages(validationResult.getErrorMessages());
-            return false;
-        }
-        User user = userDto.getUser();
-        User existingUser = this.userDao.getByUsername(user.getUsername());
-
-        if (existingUser != null
-                && !existingUser.isDeleted()
-                && !existingUser.getId().equals(user.getId())) {
-            result.addErrorMessage("Ya existe otro usuario con el mismo nombre de usuario.");
-        }
-
-        if (existingUser.isDeleted()) {
-            result.addErrorMessage("El usuario fue borrado con anterioridad.");
-            return false;
-        }
-
-        if (user.hasRole("ROLE_ADMIN") && Strings.isNullOrEmpty(user.getEmailAddress())) {
-            result.formatErrorMessage(
-                    "Un usuario de tipo administrador necesita tener definida"
-                    + " una cuenta de correo electrónico.");
-            return false;
-        }
-
-        return true;
     }
 
     @Override
@@ -148,7 +140,7 @@ public class UserManagementServiceImpl
             // We are changing the flag for an existing user so we don't
             // need to provide the encoded password.
             userDao.save(persistedUser, null);
-            res.setResult(dtoFactory.create(persistedUser));
+            res.setResult(getDtoFactory().create(persistedUser));
         }
 
         return res;
@@ -174,7 +166,7 @@ public class UserManagementServiceImpl
                 userDao.save(currentUser, passwordEncoder.encodePassword(
                         newPassword, null));
 
-                result.setResult(dtoFactory.create(currentUser));
+                result.setResult(getDtoFactory().create(currentUser));
             }
 
         } else {
@@ -193,12 +185,12 @@ public class UserManagementServiceImpl
         Result<List<UserDto>> result = new Result<List<UserDto>>();
         try {
             List<User> users = userDao.getAllNotDeletedUsers();
-            
+
             result.setResult(Lists.apply(users, new Applicable<User, UserDto>() {
 
                 @Override
                 public UserDto apply(User element) {
-                    return dtoFactory.create(element);
+                    return getDtoFactory().create(element);
                 }
             }));
         } catch (Exception ex) {
@@ -217,24 +209,22 @@ public class UserManagementServiceImpl
 
         return result;
     }
-    // </editor-fold>
 
     @Override
-    public Result save(UserDto userDto) {
+    public Result<UserDto> save(UserFormDto userDto) {
 
         Result<UserDto> result = new Result<UserDto>();
 
-        if (!validateUserSaving(userDto, result)) {
+        User user = validateUserSaving(userDto, result);
+        if (!result.isSuccess()) {
             return result;
         }
-
-        User user = userDto.getUser();
 
         try {
             userDao.save(user,
                     passwordEncoder.encodePassword(user.getPassword(), null));
 
-            UserDto dUser = dtoFactory.create(user);
+            UserDto dUser = getDtoFactory().create(user);
             result.setResult(dUser);
         } catch (RuntimeException re) {
             result.addErrorMessage("Ocurrió un error de base de datos.");
@@ -242,5 +232,47 @@ public class UserManagementServiceImpl
 
 
         return result;
+    }
+
+    private User validateUserSaving(UserDto userDto, Result result) {
+        FieldsValidationResult validationResult = fieldsValidator.validate(
+                userDto);
+
+        if (!validationResult.isValid()) {
+            result.addErrorMessages(validationResult.getErrorMessages());
+            return null;
+        }
+
+
+        User existingUser = this.userDao.getByUsername(userDto.getUsername());
+
+        if (existingUser != null
+                && !existingUser.isDeleted()
+                && !existingUser.getId().equals(userDto.getId())) {
+            result.addErrorMessage("Ya existe otro usuario con el mismo nombre de usuario.");
+        }
+
+        if (existingUser.isDeleted()) {
+            result.addErrorMessage("El usuario fue borrado con anterioridad.");
+            return null;
+        }
+
+        return existingUser;
+    }
+    // </editor-fold>
+
+    private User getUserById(Long userId, Result r) {
+        User user = null;
+        try {
+            user = userDao.getById(userId);
+
+        } catch (RuntimeException e) {
+            r.addErrorMessage("Ocurrió un error en la base de datos.");
+        }
+
+        if (user == null) {
+            r.addErrorMessage("No se encontró el usuario especificado.");
+        }
+        return user;
     }
 }
