4ed36f55c26fa45390ffacc372c6be0afd6e9353
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2011-07-21 16:49:41 +0000

    * Made it implement Labelled.

--- GreenRiverCommonsSpring/src/com/greenriver/commons/web/services/userManagement/UserDto.java
+++ GreenRiverCommonsSpring/src/com/greenriver/commons/web/services/userManagement/UserDto.java
@@ -1,11 +1,13 @@
 
 package com.greenriver.commons.web.services.userManagement;
 
+import com.greenriver.commons.data.Labelled;
+
 /**
  * Interface that UserDtos need to implement.
  * @author luisro
  */
-public interface UserDto {
+public interface UserDto extends Labelled {
    Long getId();
    
    public String getUsername();
