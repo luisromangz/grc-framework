6437351a8cb7a9bd2714fd4512bb12bccabca53c
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2011-07-25 10:04:44 +0000

    * Revamped QueryArgs estructure. QueryArgs now doesnt need to be extended, it is just a dto used to transfer query data and restrictions. * Query properties are defined in the entity class using annotations @QueryArgsProperties (in the class) and @QueryArgsField (in the fields). * Removed many unused things. * Added CRUDDao interface. * UserDao now extends CRUDDao.

---
+++ GreenRiverCommons/src/com/greenriver/commons/data/dao/CRUDDao.java
@@ -0,0 +1,17 @@
+package com.greenriver.commons.data.dao;
+
+import com.greenriver.commons.data.DataEntity;
+import com.greenriver.commons.data.dao.queryArgs.QueryArgs;
+import java.util.List;
+
+/**
+ *
+ * @author luisro
+ */
+public interface CRUDDao<E extends DataEntity, Q extends QueryArgs> {
+    public E get(E entity);
+    public E getById(Long id);
+    public void save(E entity);
+    public void remove(E entity);
+    public List<E> query(Q qArgs);
+}
---
+++ GreenRiverCommons/src/com/greenriver/commons/data/dao/queryArgs/QueryArgs.java
@@ -0,0 +1,115 @@
+package com.greenriver.commons.data.dao.queryArgs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Common query arguments stuff
+ * @author Miguel Angel
+ */
+public class QueryArgs {
+
+    private int first;
+    private int count;
+    private String textFilter = "";
+    private String sortFieldName;
+    private boolean sortAscending;
+    
+    private List<QueryArgsRestriction> restrictions;
+
+    public QueryArgs() {
+        restrictions = new ArrayList<QueryArgsRestriction>();
+    }        
+
+    //<editor-fold defaultstate="collapsed" desc="Getters and setters">
+    /**
+     * @return the fieldName
+     */
+    public String getSortFieldName() {
+        return sortFieldName;
+    }
+    
+    /**
+     * @param fieldName the fieldName to set
+     */
+    public void setSortFieldName(String fieldName) {
+        this.sortFieldName = fieldName;
+    }
+    
+    /**
+     * @return the ascending
+     */
+    public boolean isSortAscending() {
+        return sortAscending;
+    }
+    
+    /**
+     * @param ascending the ascending to set
+     */
+    public void setSortAscending(boolean ascending) {
+        this.sortAscending = ascending;
+    }
+    
+    /**
+     * @return the textFilter
+     */
+    public String getTextFilter() {
+        return textFilter;
+    }
+    
+    /**
+     * @param textFilter the textFilter to set
+     */
+    public void setTextFilter(String textFilter) {
+        this.textFilter = textFilter.trim();
+    }
+
+    
+    
+    /**
+     * @return the count
+     */
+    public int getCount() {
+        return count;
+    }
+
+    /**
+     * @param count the count to set
+     */
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    /**
+     * @return the first
+     */
+    public int getFirst() {
+        return first;
+    }
+
+    /**
+     * @param first the first to set
+     */
+    public void setFirst(int first) {
+        this.first = first;
+    }
+    
+    /**
+     * @return the restrictions
+     */
+    public List<QueryArgsRestriction> getRestrictions() {
+        return restrictions;
+    }
+
+    /**
+     * @param restrictions the restrictions to set
+     */
+    public void setRestrictions(List<QueryArgsRestriction> restrictions) {
+        this.restrictions = restrictions;
+    }
+    
+    //</editor-fold>
+
+   
+
+}
