221955f526a1f03a18ee365265f0a6bd4d5833cb
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2011-07-25 12:27:22 +0000

    * Changed implemented interface name.

--- GreenRiverCommonsSpring/src/com/greenriver/commons/web/services/userManagement/UserDto.java
+++ GreenRiverCommonsSpring/src/com/greenriver/commons/web/services/userManagement/UserDto.java
@@ -1,13 +1,14 @@
 
 package com.greenriver.commons.web.services.userManagement;
 
-import com.greenriver.commons.data.Labelled;
+import com.greenriver.commons.data.DataEntity;
 
 /**
  * Interface that UserDtos need to implement.
  * @author luisro
  */
-public abstract class UserDto implements Labelled {
+public abstract class UserDto implements DataEntity {
+    @Override
    public abstract Long getId();
    
    public abstract String getUsername();
