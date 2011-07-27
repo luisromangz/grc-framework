6c06e32c56bc842e8c92d0767fae77d39afdfb0b
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2011-07-27 08:35:25 +0000

    * Simplified UserDto creation, no longer requiring factories.

--- trunk/GreenRiverCommonsSpring/src/com/greenriver/commons/web/services/userManagement/UserDto.java
+++ trunk/GreenRiverCommonsSpring/src/com/greenriver/commons/web/services/userManagement/UserDto.java
@@ -1,27 +1,29 @@
-
 package com.greenriver.commons.web.services.userManagement;
 
 import com.greenriver.commons.data.DataEntity;
+import com.greenriver.commons.data.model.User;
 
 /**
  * Interface that UserDtos need to implement.
  * @author luisro
  */
-public abstract class UserDto implements DataEntity {
-    private boolean newEntity=false;
-    
-    
-    @Override
-   public abstract Long getId();
-   
-   public abstract String getUsername();
-   
-   public boolean isNewEntity() {
-       return newEntity;
-   }
-   
-   public void setNewEntity(boolean newEntity) {
-       this.newEntity = newEntity;
-   }
-}
+public abstract class UserDto
+        implements DataEntity {
 
+    private boolean newEntity = false;
+
+    @Override
+    public abstract Long getId();
+
+    public abstract String getUsername();
+
+    public boolean isNewEntity() {
+        return newEntity;
+    }
+
+    public void setNewEntity(boolean newEntity) {
+        this.newEntity = newEntity;
+    }
+
+    public abstract void fromUser(User user, boolean forGrid);
+}
--- trunk/GreenRiverCommonsSpring/src/com/greenriver/commons/web/services/userManagement/UserFormDto.java
+++ trunk/GreenRiverCommonsSpring/src/com/greenriver/commons/web/services/userManagement/UserFormDto.java
@@ -1,12 +1,30 @@
 
 package com.greenriver.commons.web.services.userManagement;
 
+import com.greenriver.commons.data.DataEntity;
 import com.greenriver.commons.data.model.User;
 
 /**
  * Interface that UserDtos need to implement.
  * @author luisro
  */
-public abstract class UserFormDto extends UserDto {
+public abstract class UserFormDto implements DataEntity{
+    private boolean newEntity = false;
+
+    @Override
+    public abstract Long getId();
+
+    public abstract String getUsername();
+
+    public boolean isNewEntity() {
+        return newEntity;
+    }
+
+    public void setNewEntity(boolean newEntity) {
+        this.newEntity = newEntity;
+    }
+
+    public abstract void fromUser(User user);
+    
     public abstract void copyTo(User user);
 }
