4bcdfc59e93866908d4321cc9feb91e243641f00
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2011-08-05 15:54:26 +0000

    * Refactored UserManagementServiceImpl into CRUDServiceImpl so we have a base for generic CRUD services.

--- GreenRiverCommonsSpring/src/com/greenriver/commons/web/services/crud/CRUDService.java
+++ GreenRiverCommonsSpring/src/com/greenriver/commons/web/services/crud/CRUDService.java
@@ -1,6 +1,10 @@
-package com.greenriver.commons.web.services;
+package com.greenriver.commons.web.services.crud;
 
 import com.greenriver.commons.data.dao.queryArgs.QueryArgs;
+import com.greenriver.commons.web.services.Dto;
+import com.greenriver.commons.web.services.FormDto;
+import com.greenriver.commons.web.services.PagedResult;
+import com.greenriver.commons.web.services.Result;
 
 /**
  * This interface defines the methods a service ofering CRUD operations for 
@@ -13,7 +17,7 @@ public interface CRUDService <D extends Dto,F extends FormDto>{
     Result<D> getForView(Long id);
     Result<F> getForEdit(Long id);
     
-    Result<D> save(F item);
+    //Result<D> save(F item);
     Result<D> remove(Long id);
     
     PagedResult<D> query(QueryArgs args);
