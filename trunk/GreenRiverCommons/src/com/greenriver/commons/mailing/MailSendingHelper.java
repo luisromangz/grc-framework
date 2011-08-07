6dff247c3ebeb67ef1326f537286b7108288e121
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2011-08-07 17:06:42 +0000

    * Added methods to MailSenderHelper so we have more possibilites in the client.

--- trunk/GreenRiverCommons/src/com/greenriver/commons/mailing/MailSendingHelper.java
+++ trunk/GreenRiverCommons/src/com/greenriver/commons/mailing/MailSendingHelper.java
@@ -2,6 +2,7 @@ package com.greenriver.commons.mailing;
 
 import com.greenriver.commons.ErrorMessagesException;
 import com.greenriver.commons.data.mailing.Mail;
+import com.greenriver.commons.data.mailing.MailServerConfig;
 
 /**
  * Interface for a helper that allows sending mails.
@@ -9,5 +10,7 @@ import com.greenriver.commons.data.mailing.Mail;
  */
 public interface MailSendingHelper {
     
+    MailServerConfig getMailServerConfig() throws ErrorMessagesException;
     boolean sendHtmlMail(Mail mail) throws ErrorMessagesException;
+    boolean sendHtmlMail(Mail mail, MailServerConfig config) throws ErrorMessagesException;
 }
