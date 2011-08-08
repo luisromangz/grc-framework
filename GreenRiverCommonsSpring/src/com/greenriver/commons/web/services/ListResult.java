54aa6ce88dce3cd78059d91bbfe2af44baf93d11
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2011-08-08 08:35:06 +0000

    * Added two new service result types.

---
+++ GreenRiverCommonsSpring/src/com/greenriver/commons/web/services/ListResult.java
@@ -0,0 +1,25 @@
+package com.greenriver.commons.web.services;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Defines a service result encapsultator for services returning lists.
+ * 
+ * @author luisro
+ */
+public class ListResult <T> extends Result<List<T>>{
+
+    public ListResult() {
+        this.setResult(new ArrayList<T>());
+    }
+    
+    public void add(T element) {
+        this.getResult().add(element);                
+    }
+    
+    public void addAll(Collection<T> collection) {
+        this.getResult().addAll(collection);
+    }
+}
---
+++ GreenRiverCommonsSpring/src/com/greenriver/commons/web/services/MapResult.java
@@ -0,0 +1,25 @@
+
+package com.greenriver.commons.web.services;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A service result encapsultaror for methods returning maps.
+ * @author luisro
+ */
+public class MapResult<K,V> extends Result<Map<K,V>> {
+
+    public MapResult() {
+        this.setResult(new HashMap<K,V>());
+    }
+    
+    public void put(K key, V value) {
+        this.getResult().put(key, value);
+    }
+    
+    public void putAll(Map<K,V> values) {
+        this.getResult().putAll(values);
+    }
+    
+}
