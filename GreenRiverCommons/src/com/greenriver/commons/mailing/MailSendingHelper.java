fd42bde632b957d395e6443d0dbe3292fa0c61af
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2011-08-19 18:03:04 +0000

    Added background thread mailer util to MailSendingHelper.

--- GreenRiverCommons/src/com/greenriver/commons/mailing/MailSendingHelper.java
+++ GreenRiverCommons/src/com/greenriver/commons/mailing/MailSendingHelper.java
@@ -13,4 +13,29 @@ public interface MailSendingHelper {
     MailServerConfig getMailServerConfig() throws ErrorMessagesException;
     boolean sendHtmlMail(Mail mail) throws ErrorMessagesException;
     boolean sendHtmlMail(Mail mail, MailServerConfig config) throws ErrorMessagesException;
+    
+     class BackgroundMailer implements Runnable {
+        Mail mail;
+        MailServerConfig config;
+        MailSendingHelper helper;
+        
+        public BackgroundMailer(
+                Mail mail, 
+                MailServerConfig config,
+                MailSendingHelper helper) {
+            this.mail = mail;
+            this.config = config;
+            this.helper = helper;
+        }
+
+        @Override
+        public void run() {
+            try {
+                helper.sendHtmlMail(this.mail,this.config);
+            } catch (ErrorMessagesException ex) {
+                // We dont log as we have logged the errors in the helper.
+            }
+        }
+        
+    }
 }
