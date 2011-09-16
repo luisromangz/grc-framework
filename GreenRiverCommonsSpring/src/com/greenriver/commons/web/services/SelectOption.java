a80c4ec139762b733ab269e8dc2adc0632b2ae29
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2011-09-16 17:21:44 +0000

    * Removed extra props field as it confused filtering selects. * Added tag field. * Implements Comparable.

--- GreenRiverCommonsSpring/src/com/greenriver/commons/web/services/SelectOption.java
+++ GreenRiverCommonsSpring/src/com/greenriver/commons/web/services/SelectOption.java
@@ -4,44 +4,70 @@ package com.greenriver.commons.web.services;
  *
  * @author luisro
  */
-public class SelectOption {
+public class SelectOption implements Comparable<SelectOption> {
     private String label;
     private Object value;
     
+    private String tag="";
+    
     public SelectOption() {
-        
     }
 
     public SelectOption(Object value, String label) {
+        this();
         this.value = value;
         this.label = label;
     }
 
+    @Override
+    public int compareTo(SelectOption t) {
+        return this.label.compareTo(t.getLabel());
+    }
+    
+    
+
+    //<editor-fold defaultstate="collapsed" desc="Getters and setters">
     /**
      * @return the label
      */
     public String getLabel() {
         return label;
     }
-
+    
     /**
      * @param label the label to set
      */
     public void setLabel(String label) {
         this.label = label;
     }
-
+    
     /**
      * @return the value
      */
     public Object getValue() {
         return value;
     }
-
+    
     /**
      * @param value the value to set
      */
     public void setValue(Object value) {
         this.value = value;
     }
+    
+    /**
+     * @return the tag
+     */
+    public String getTag() {
+        return tag;
+    }
+    
+    /**
+     * @param tag the tag to set
+     */
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+    //</editor-fold>
+
 }
