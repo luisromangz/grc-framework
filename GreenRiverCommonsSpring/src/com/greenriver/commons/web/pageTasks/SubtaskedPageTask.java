6fa5d0d8d78a16659d5854b5bb83372e62b77ef9
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2011-09-27 18:17:39 +0000

    Returns own form, grids and props views together with its children's (which are prefixed).

--- GreenRiverCommonsSpring/src/com/greenriver/commons/web/pageTasks/SubtaskedPageTask.java
+++ GreenRiverCommonsSpring/src/com/greenriver/commons/web/pageTasks/SubtaskedPageTask.java
@@ -1,15 +1,17 @@
 package com.greenriver.commons.web.pageTasks;
 
 import com.greenriver.commons.web.configuration.PageTasksContainer;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * This class represents a PageTask that will be formed by several subtasks.
  * @author luisro
  */
-public class SubtaskedPageTask 
-    extends DojoHandledPageTask
-    implements PageTasksContainer {
+public class SubtaskedPageTask
+        extends DojoHandledPageTask
+        implements PageTasksContainer {
 
     PageTaskManager taskManager;
 
@@ -17,11 +19,62 @@ public class SubtaskedPageTask
         super();
 
         taskManager = new PageTaskManager();
-        
+
         this.setMainJspFileName("../../subtaskedPageTask.jsp");
         this.setDojoControllerModule("grc.web.tasks.SubtaskedPageTaskController");
     }
+
+    @Override
+    public Map<String, String> getGrids() {
+        Map<String, String> grids = new HashMap<String, String>();
+        Map<String, String> ownGrids = super.getGrids();
+
+        grids.putAll(ownGrids);
+
+
+        for (PageTask t : this.getPageTasks()) {
+            addAndPrefixKeys(t.getTaskName(), t.getGrids(), grids);
+        }
+
+        return grids;
+    }
+
+    @Override
+    public Map<String, String> getForms() {
+        Map<String, String> forms = new HashMap<String, String>();
+        Map<String, String> ownForms = super.getForms();
+
+        forms.putAll(ownForms);
+
+
+        for (PageTask t : this.getPageTasks()) {
+            addAndPrefixKeys(t.getTaskName(), t.getForms(), forms);
+        }
+
+        return forms;
+    }
+
+    @Override
+    public Map<String, String> getPropsViews() {
+        Map<String, String> propViews = new HashMap<String, String>();
+        Map<String, String> ownPropsViews = super.getGrids();
+
+        propViews.putAll(ownPropsViews);
+
+
+        for (PageTask t : this.getPageTasks()) {
+            addAndPrefixKeys(t.getTaskName(), t.getPropsViews(), propViews);
+        }
+
+        return propViews;
+    }
     
+
+    private void addAndPrefixKeys(String prefix, Map<String, String> source, Map<String, String> result) {
+        for (String key : source.keySet()) {
+            result.put(prefix + "_" + key, source.get(key));
+        }
+    }
 
     // <editor-fold defaultstate="collapsed" desc="Getters and setters">
     @Override
@@ -31,13 +84,12 @@ public class SubtaskedPageTask
 
     @Override
     public void setPageTasks(List<PageTask> pageTasks) {
-       taskManager.setTasks(pageTasks);
+        taskManager.setTasks(pageTasks);
     }
 
     @Override
     public PageTask getTask(String taskName) {
-       return taskManager.getTask(taskName);
+        return taskManager.getTask(taskName);
     }
     // </editor-fold>
-
 }
