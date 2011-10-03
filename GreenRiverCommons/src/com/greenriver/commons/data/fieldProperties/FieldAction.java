d0cd953d6b5d16211dfaa2e505f2f2d7231173a6
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2011-10-03 16:10:05 +0000

    * Added docs. * Removed class.

--- GreenRiverCommons/src/com/greenriver/commons/data/fieldProperties/FieldAction.java
+++ GreenRiverCommons/src/com/greenriver/commons/data/fieldProperties/FieldAction.java
@@ -14,10 +14,37 @@ import java.lang.annotation.Target;
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface FieldAction {
+    /**
+     * The name of the field that will be modified.
+     * @return 
+     */
     public String targetField() default "";
+    /**
+     * The name of the field that triggers the change.
+     * @return 
+     */
     public String triggerField();
+    /**
+     * The value that triggers the change.
+     * @return 
+     */
     public String triggerValue();
+    /**
+     * If true, the action will be triggered when the field's value is equal to
+     * the specified value. Else, it will be triggered when the value is not equal.
+     * @return 
+     */
     public boolean equals() default true;
+    /**
+     * The value to set in the target field when the action is triggered.
+     * @return 
+     */
     public String newValue() default  "\0";
+    /**
+     * If the field must be deactivated when the condition is true. It will be
+     * activated when the condition is false.
+     * 
+     * @return 
+     */
     public boolean deactivate() default false;
 }
--- GreenRiverCommons/src/com/greenriver/commons/data/fieldProperties/FieldActions.java
+++ GreenRiverCommons/src/com/greenriver/commons/data/fieldProperties/FieldActions.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.greenriver.commons.data.fieldProperties;
 
 import java.lang.annotation.Documented;
@@ -11,7 +7,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- *
+ * This annotation allows the use of various @FieldAction in a field.
  * @author luisro
  */
 @Documented
