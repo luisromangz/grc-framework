791dbaae04403acf59c57afa19ec23900b08f44f
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2011-11-08 17:57:25 +0000

    We use ImageMagick as the previous solution gave very little quality.

--- GreenRiverCommons/src/com/greenriver/commons/pdf/PDFUtils.java
+++ GreenRiverCommons/src/com/greenriver/commons/pdf/PDFUtils.java
@@ -1,16 +1,7 @@
 package com.greenriver.commons.pdf;
 
-
-import com.sun.pdfview.PDFFile;
-import com.sun.pdfview.PDFPage;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.ByteBuffer;
-import javax.imageio.ImageIO;
-import org.apache.commons.fileupload.util.Streams;
+import org.im4java.core.ConvertCmd;
+import org.im4java.core.IMOperation;
 
 /**
  * This class contains utilities for handling PDF files.
@@ -18,24 +9,15 @@ import org.apache.commons.fileupload.util.Streams;
  */
 public class PDFUtils {
 
-    public static PDFFile getPdfFile(InputStream pdfFile) throws IOException {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        Streams.copy(pdfFile, out, true);
-        ByteBuffer buf = ByteBuffer.wrap(out.toByteArray());
-
-        PDFFile file = new PDFFile(buf);
-
-        return file;
-    }
-
-    public static void pdfPageToImage(
-            PDFPage page, String imageFormat, OutputStream outputStream) throws IOException {
-
-        BufferedImage img = (BufferedImage) page.getImage(
-                (int) page.getWidth(),
-                (int) page.getHeight(),
-                page.getPageBox(), null, true, true);
-
-        ImageIO.write(img, "png", outputStream);
+    public static void pdfToImages(String filePath, String resultPath) throws Exception{
+         ConvertCmd cmd = new ConvertCmd();
+         
+         IMOperation op = new IMOperation();
+         
+         op.density(120);
+         op.addImage(filePath);
+         op.addImage(resultPath);
+         
+         cmd.run(op);
     }
 }
