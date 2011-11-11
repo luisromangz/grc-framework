50c6571396dad2e031d3101e3029695185e3d78e
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2011-11-11 14:30:02 +0000

    Classes made static.

--- GreenRiverCommons/src/com/greenriver/commons/pdf/PrintableDocumentToPDFConverter.java
+++ GreenRiverCommons/src/com/greenriver/commons/pdf/PrintableDocumentToPDFConverter.java
@@ -10,6 +10,7 @@ import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
+import javax.xml.crypto.dsig.spec.HMACParameterSpec;
 import org.apache.log4j.Logger;
 import org.w3c.tidy.Tidy;
 
@@ -18,43 +19,42 @@ import org.w3c.tidy.Tidy;
  * @author luisro
  */
 public class PrintableDocumentToPDFConverter {
-    private HTMLToPDFConverter htmlToPDFConverter;
 
-    private String template;
-    private String footerAndHeaderTemplate;
-    private final String PAGE_COUNTER = "<span class=\"pageCounter\"/>";
-    private final String PAGE_TOTAL = "<span class=\"totalPages\"/>";
-    private final String PAGE_SEPARATOR = "<div style=\"page-break-after:always\"><!--Non empty--></div>";
+    private static String template;
+    private static String footerAndHeaderTemplate;
+    private static final String PAGE_COUNTER = "<span class=\"pageCounter\"/>";
+    private static final String PAGE_TOTAL = "<span class=\"totalPages\"/>";
+    private static final String PAGE_SEPARATOR = "<div style=\"page-break-after:always\"><!--Non empty--></div>";
 
-    public void convert(PrintableDocument document, OutputStream pdfOutput) {
+    public static void convert (PrintableDocument document, OutputStream pdfOutput) {
         ArrayList<PrintableDocument> list = new ArrayList<PrintableDocument>();
         list.add(document);
-        this.convert(list, pdfOutput);
+        convert(list, pdfOutput);
     }
 
-    public void convert(List<PrintableDocument> documents, OutputStream pdfOutput) {
+    public static void convert(List<PrintableDocument> documents, OutputStream pdfOutput) {
         Logger logger = Logger.getLogger("grc-commons");
         try {
             template = Strings.fromInputStream(
-                    this.getClass().getResourceAsStream("printingTemplate.xhtml"));
+                    PrintableDocumentToPDFConverter.class.getResourceAsStream("printingTemplate.xhtml"));
             footerAndHeaderTemplate = Strings.fromInputStream(
-                    this.getClass().getResourceAsStream("footerAndHeaderTemplate.xml"));
+                    PrintableDocumentToPDFConverter.class.getResourceAsStream("footerAndHeaderTemplate.xml"));
 
-        } catch (IOException ex) {           
-            logger.error(ex,ex.getCause());
+        } catch (IOException ex) {
+            logger.error(ex, ex.getCause());
             throw new RuntimeException("A template wasn't found.", ex);
         }
 
         String html = fillTemplate(documents);
-        try {           
-            htmlToPDFConverter.convertToPDF(new ByteArrayInputStream(html.getBytes("UTF-8")), pdfOutput);           
+        try {
+            HTMLToPDFConverter.convertToPDF(new ByteArrayInputStream(html.getBytes("UTF-8")), pdfOutput);
         } catch (Exception e) {
             logger.error(html, e.getCause());
             throw new RuntimeException("There was a failure filling the template", e);
         }
     }
 
-      private String fillTemplate(List<PrintableDocument> documents) {
+    private static String fillTemplate(List<PrintableDocument> documents) {
         // All documents share template so share all info.
         PrintableDocument firstDocument = documents.get(0);
         String html = template.replace("%TITLE%", firstDocument.getTitle());
@@ -99,28 +99,12 @@ public class PrintableDocumentToPDFConverter {
             tidiedBodies.add(tidiedWriter.toString());
         }
 
-        html = html.replace("%DOCUMENT_BODY%", Strings.join(tidiedBodies, this.PAGE_SEPARATOR));
+        html = html.replace("%DOCUMENT_BODY%", Strings.join(tidiedBodies, PAGE_SEPARATOR));
 
         return html;
     }
 
-    private String replaceFooter(String footer) {
-        return footer.replace("%TOTAL_PAGINAS%", this.PAGE_TOTAL).replace("%PAGINA%", this.PAGE_COUNTER);
+    private static String replaceFooter(String footer) {
+        return footer.replace("%TOTAL_PAGINAS%", PAGE_TOTAL).replace("%PAGINA%", PAGE_COUNTER);
     }
-
-    // <editor-fold defaultstate="collapsed" desc="Getters and setters">
-    /**
-     * @return the htmlToPDFConverter
-     */
-    public HTMLToPDFConverter getHtmlToPDFConverter() {
-        return htmlToPDFConverter;
-    }
-
-    /**
-     * @param htmlToPDFConverter the htmlToPDFConverter to set
-     */
-    public void setHtmlToPDFConverter(HTMLToPDFConverter htmlToPDFConverter) {
-        this.htmlToPDFConverter = htmlToPDFConverter;
-    }
-    // </editor-fold>
 }
