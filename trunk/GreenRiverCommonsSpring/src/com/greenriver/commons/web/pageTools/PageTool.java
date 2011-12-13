23879c308cc98c8d2a0da1750d96cb75042dc8c0
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2011-12-13 20:06:06 +0000

    One can now configure grids for a tool.

--- trunk/GreenRiverCommonsSpring/src/com/greenriver/commons/web/pageTools/PageTool.java
+++ trunk/GreenRiverCommonsSpring/src/com/greenriver/commons/web/pageTools/PageTool.java
@@ -2,6 +2,7 @@ package com.greenriver.commons.web.pageTools;
 
 import com.greenriver.commons.data.model.User;
 import com.greenriver.commons.web.configuration.FormsContainer;
+import com.greenriver.commons.web.configuration.GridsContainer;
 import com.greenriver.commons.web.configuration.PageConfig;
 import com.greenriver.commons.web.configuration.PropsViewsContainer;
 import com.greenriver.commons.web.helpers.header.HeaderConfig;
@@ -15,11 +16,10 @@ import java.util.Map;
  * @author luis
  */
 public class PageTool
-        implements FormsContainer, PropsViewsContainer, HeaderConfig {
+        implements FormsContainer, PropsViewsContainer, GridsContainer, HeaderConfig {
 
     // <editor-fold defaultstate="collapsed" desc="Attributes">
     private String[] allowedRoles = {"ROLE_USER"};
-    
     /**
      * The tool's name.
      */
@@ -33,8 +33,6 @@ public class PageTool
      */
     private List<String> setupPaneJspFiles;
     private PageConfig pageConfiguration;
-
-
     private boolean initialized = false;
     // </editor-fold>
 
@@ -55,22 +53,29 @@ public class PageTool
         this.internalInitialize();
     }
     
-    protected void internalInitialize(){
+    protected void internalInitialize() {
         
     }
     
     public final boolean isInitialized() {
         return initialized;
     }
-
+    
     @Override
     public boolean equals(Object o) {
-        if(o.getClass() != PageTool.class) {
+        if (o.getClass() != PageTool.class) {
             return false;
         }
-
-        String oName = ((PageTool)o).getName();
+        
+        String oName = ((PageTool) o).getName();
         return oName.equals(this.name);
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + (this.name != null ? this.name.hashCode() : 0);
+        return hash;
     }
 
     // <editor-fold defaultstate="collapsed" desc="Getters & setters">
@@ -115,7 +120,7 @@ public class PageTool
     public void setSetupPaneJspFiles(List<String> setupPaneJspFiles) {
         this.setupPaneJspFiles = setupPaneJspFiles;
     }
-
+    
     @Override
     public List<String> getJavaScriptFiles() {
         return this.pageConfiguration.getJavaScriptFiles();
@@ -128,153 +133,151 @@ public class PageTool
     public void setJavaScriptFiles(List<String> javaScriptFiles) {
         this.pageConfiguration.setJavaScriptFiles(javaScriptFiles);
     }
-
+    
     @Override
     public void addForm(String id, String className) {
         this.pageConfiguration.addForm(id, className);
     }
-
+    
     @Override
     public Map<String, String> getForms() {
         return pageConfiguration.getForms();
     }
-
+    
     @Override
     public void setForms(Map<String, String> forms) {
         pageConfiguration.setForms(forms);
     }
-
+    
     @Override
     public void addPropsView(String id, String className) {
         pageConfiguration.addPropsView(id, className);
     }
-
+    
     @Override
     public void setPropsViews(Map<String, String> className) {
         pageConfiguration.setPropsViews(className);
     }
-
+    
     @Override
     public Map<String, String> getPropsViews() {
         return pageConfiguration.getPropsViews();
     }
-
+    
     @Override
     public void addCssFile(String cssFilename) {
         pageConfiguration.addCssFile(cssFilename);
     }
-
+    
     @Override
     public void addDwrService(String name) {
         pageConfiguration.addDwrService(name);
     }
-
+    
     @Override
     public void addDojoBundle(String bundleName) {
         pageConfiguration.addDojoBundle(bundleName);
     }
-
+    
     @Override
     public void addDojoModule(String dojoModule) {
         pageConfiguration.addDojoModule(dojoModule);
     }
-
+    
     @Override
     public void addJavaScriptFile(String jsFilename) {
         pageConfiguration.addJavaScriptFile(jsFilename);
     }
-
+    
     @Override
     public void addOnLoadScript(String code) {
         pageConfiguration.addOnLoadScript(code);
     }
-
+    
     @Override
     public void addScript(String script) {
         pageConfiguration.addScript(script);
     }
-
+    
     @Override
     public List<String> getCssFiles() {
         return pageConfiguration.getCssFiles();
     }
-
+    
     @Override
     public List<String> getDojoModules() {
         return pageConfiguration.getDojoModules();
     }
-
+    
     @Override
     public List<String> getDojoBundles() {
         return pageConfiguration.getDojoBundles();
     }
-
+    
     @Override
     public List<String> getDwrServices() {
         return pageConfiguration.getDwrServices();
     }
-
+    
     @Override
     public List<String> getOnLoadScripts() {
         return pageConfiguration.getOnLoadScripts();
     }
-
+    
     @Override
     public List<String> getScripts() {
         return pageConfiguration.getScripts();
     }
-
+    
     @Override
     public String getTitle() {
         return pageConfiguration.getTitle();
     }
-
+    
     @Override
     public void setTitle(String title) {
         pageConfiguration.setTitle(title);
     }
-
+    
     @Override
     public void setCssFiles(List<String> cssFiles) {
         pageConfiguration.setCssFiles(cssFiles);
     }
-
+    
     @Override
     public void setDwrServices(List<String> dwrServices) {
         pageConfiguration.setDwrServices(dwrServices);
     }
-
+    
     @Override
     public void setDojoBundles(List<String> dojoBundles) {
         pageConfiguration.setDojoBundles(dojoBundles);
     }
-
+    
     @Override
     public void setDojoModules(List<String> dojoModules) {
         pageConfiguration.setDojoModules(dojoModules);
     }
-
+    
     @Override
     public void setOnLoadScripts(List<String> onLoadScripts) {
         pageConfiguration.setOnLoadScripts(onLoadScripts);
     }
-
+    
     @Override
     public void setScripts(List<String> scripts) {
         pageConfiguration.setScripts(scripts);
     }
-
+    
     @Override
     public void addDojoBundles(List<String> dojoBundles) {
         pageConfiguration.addDojoBundles(dojoBundles);
     }
-
+    
     @Override
     public void addDojoModules(List<String> dojoModules) {
         pageConfiguration.addDojoModules(dojoModules);
     }
-
-   // </editor-fold>
 
     /**
      * @return the allowedRoles
@@ -289,6 +292,20 @@ public class PageTool
     public void setAllowedRoles(String[] allowedRoles) {
         this.allowedRoles = allowedRoles;
     }
+    
+    @Override
+    public Map<String, String> getGrids() {
+        return pageConfiguration.getGrids();
+    }
+    
+    @Override
+    public void setGrids(Map<String, String> grids) {
+        pageConfiguration.setGrids(grids);
+    }
+    
+    @Override
+    public void addGrid(String id, String className) {
+        pageConfiguration.addGrid(id, className);
+    }
+    // </editor-fold>
 }
-
-
