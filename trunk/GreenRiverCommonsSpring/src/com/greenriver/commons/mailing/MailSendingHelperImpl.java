542f423132e66982aac6a38bcabc123ff09dcda5
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2012-01-14 14:41:01 +0000

    Code cleaning.

--- trunk/GreenRiverCommonsSpring/src/com/greenriver/commons/mailing/MailSendingHelperImpl.java
+++ trunk/GreenRiverCommonsSpring/src/com/greenriver/commons/mailing/MailSendingHelperImpl.java
@@ -10,19 +10,12 @@ import java.io.IOException;
 import java.util.Date;
 import java.util.Properties;
 import javax.activation.DataHandler;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
-import javax.mail.NoSuchProviderException;
-import javax.mail.Session;
-import javax.mail.Transport;
+import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
-import javax.mail.util.ByteArrayDataSource;
 import org.apache.log4j.Logger;
-import org.springframework.format.datetime.joda.MillisecondInstantPrinter;
 
 /**
  * Implemetantion of a mail server helper, using JavaMail.
