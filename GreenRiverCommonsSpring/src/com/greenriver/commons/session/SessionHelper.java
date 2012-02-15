5845ab0484f670324441b9f6d8594d4e11d8cc36
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2012-02-15 13:02:57 +0000

    Moved class, added helper.

---
+++ GreenRiverCommonsSpring/src/com/greenriver/commons/session/SessionHelper.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.greenriver.commons.session;
+
+import javax.servlet.http.HttpSession;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+/**
+ *
+ * @author luisro
+ */
+public class SessionHelper {
+    public static HttpSession getSession() {
+        ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        HttpSession session= attr.getRequest().getSession(true);   
+        
+        return session;
+    }
+    
+    public static Object getSessionAttr(String attrName) {
+        HttpSession session = getSession();
+        return session.getAttribute(attrName);
+    }
+}
