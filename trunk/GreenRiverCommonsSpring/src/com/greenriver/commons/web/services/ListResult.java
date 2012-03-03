413350a157789beb7a6edde1cc7cf868fe431388
luisro <luisro@6107ab51-a6aa-65e5-e5e6-3f4e9e312a2c>
2012-03-03 16:59:03 +0000

    Added docs and sort methods.

--- trunk/GreenRiverCommonsSpring/src/com/greenriver/commons/web/services/ListResult.java
+++ trunk/GreenRiverCommonsSpring/src/com/greenriver/commons/web/services/ListResult.java
@@ -1,8 +1,6 @@
 package com.greenriver.commons.web.services;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /**
  * Defines a service result encapsultator for services returning lists.
@@ -15,15 +13,48 @@ public class ListResult <T> extends Result<List<T>>{
         this.setResult(new ArrayList<T>());
     }
     
+    /**
+     * Adds an element to the result collection.
+     * @param element 
+     */
     public void add(T element) {
         this.getResult().add(element);                
     }
     
+    /**
+     * Adds all elements in the given collection to the result collection.
+     * 
+     * @param collection 
+     */
     public void addAll(Collection<T> collection) {
         this.getResult().addAll(collection);
     }
 
+    /**
+     * Returns true if the given element is contained in the result collection.
+     * @param o
+     * @return 
+     */
     public boolean contains(T o) {
         return getResult().contains(o);
+    }
+
+    /**
+     * Sorts the result collection using the <c>compareTo</c> method defined
+     * in the result elements' class. If said class is not Comparable, this will
+     * fail.
+     * 
+     */
+    public void sort() {
+        this.sort(null);
+    }
+    
+    /**
+     * Sorts the result collection with the given comparator.
+     * 
+     * @param comparator 
+     */
+    public void sort(Comparator<T> comparator) {
+        Collections.sort(this.getResult(), comparator);
     }
 }
